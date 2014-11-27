1968a2fb0a0919686a1c49bbd8763d7e0d659e76
Ridhwana <Ridhwana.khan16@gmail.com>
2014-11-27 13:20:11 +0200

    WIP: handle the omniauth callback information

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -2,4 +2,28 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :omniauthable
+
+  class << self
+    def from_omniauth auth
+      user = where(provider: auth.provider, uid: auth.uid).first_or_create do |new_user|
+        new_user.provider = auth.provider
+        new_user.uid = auth.uid
+      end
+
+      update_user_info user, auth
+      user
+    end
+
+  private
+
+    def update_user_info user, auth
+      user.name = auth.info.name
+      user.email = auth.info.email
+
+      # auth.info.aliases.each do |provider_alias|
+      #   user.ad_username = provider_alias.provider_id if provider_alias.provider == 'ddad'
+      # end
+      user.save
+    end
+  end
 end
