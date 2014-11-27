32b8f82e3bae7ce1a9a4014b15f4a9ab4f37fa08
Ridhwana <Ridhwana.khan16@gmail.com>
2014-11-27 11:06:32 +0200

    feat: Configure and customize the omniauth callbacks controller. Generate and override the omniauth_callbacks_controller. Tell the router to use this controller.

---
+++ rails/app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,16 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def all
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    puts "------user attributes -------"
+    puts user.inspect
+    
+    if user.persisted?
+      sign_in_and_redirect user, notice: "Signed in!"
+      session["devise.user_info"] = request.env["omniauth.auth"]['info'] unless request.env["omniauth.auth"]['info'] == nil
+    else
+      session["devise.user_attributes"] = user.attributes
+      redirect_to new_user_registration_url
+    end
+  end
+  alias_method :isoauth2, :all
+end
