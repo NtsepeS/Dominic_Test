9812ec497d41d43d75dc0979ecbda99ad4f38dca
Ridhwana <Ridhwana.khan16@gmail.com>
2014-12-02 21:02:19 +0200

    fix: no need to reroute from index page

--- ember/app/routes/index.js
+++ ember/app/routes/index.js
@@ -1,10 +1,4 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  redirect: function() {
-    if (this.session.isAuthenticated) {
-      this.transitionTo("links");
-    }
-  }
-});
+export default Ember.Route.extend(AuthenticatedRouteMixin, {});
