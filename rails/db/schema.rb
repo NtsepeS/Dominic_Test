01d592e85742b22b228024cbca91ec6bea1e5a47
Ridhwana <Ridhwana.khan16@gmail.com>
2014-12-03 12:46:23 +0200

    feat: add the column role to the user table

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202194933) do
+ActiveRecord::Schema.define(version: 20141203100154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20141202194933) do
     t.string   "uid"
     t.string   "name"
     t.string   "ad_username"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
