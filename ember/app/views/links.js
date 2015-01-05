47df63c20f50424f874073af882d716759917fa0
Ridhwana <Ridhwana.khan16@gmail.com>
2015-01-05 14:20:50 +0200

    refactor: template name is optional if it has the same name as the view

--- ember/app/views/links.js
+++ ember/app/views/links.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  templateName: 'links',
-
   didInsertElement : function() {
     Ember.run.scheduleOnce('afterRender', this, function(){
       this.$('.js-client-links').dataTable();
