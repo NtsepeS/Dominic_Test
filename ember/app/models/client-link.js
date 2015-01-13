8e2ec4157845438f435783763517e45b307e83c4
Ridhwana <Ridhwana.khan16@gmail.com>
2015-01-13 13:27:51 +0200

    chore: stub out some more realistic data

--- ember/app/models/client-link.js
+++ ember/app/models/client-link.js
@@ -1,14 +1,28 @@
 import DS from 'ember-data';
 
 var ClientLink = DS.Model.extend({
-  name:   DS.attr('string'),
-  branch: DS.attr('string')
+  name:           DS.attr('string'),
+  branch:         DS.attr('string'),
+  circuit_number: DS.attr('string'),
+  msad_number:    DS.attr('string')
 });
 
 ClientLink.reopenClass({
   FIXTURES:[
-    {id: 1, name: 'ABC', branch: 'Johannesburg'},
-    {id: 2, name: 'DEF', branch: 'CPT'},
+    {
+      id:             1,
+      name:           'ABC',
+      branch:         'Johannesburg',
+      circuit_number: '12323-7463',
+      msad_number:    '5346513'
+    },
+    {
+      id:             2,
+      name:           'DEF',
+      branch:         'CPT',
+      circuit_number: '13-3',
+      msad_number:    '13'
+    },
   ]
 });
 
