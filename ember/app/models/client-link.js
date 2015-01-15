e691750181f2d988f6e5932b03233455813b632e
Ridhwana <Ridhwana.khan16@gmail.com>
2015-01-15 14:43:48 +0200

    feat: populate the correct fields after updating the models in Ember

--- ember/app/models/client-link.js
+++ ember/app/models/client-link.js
@@ -1,10 +1,17 @@
 import DS from 'ember-data';
 
 var ClientLink = DS.Model.extend({
-  name:          DS.attr('string'),
-  branch:        DS.attr('string'),
-  circuit_number: DS.attr('string'),
-  MSAD_number:    DS.attr('string')
+  name:                 DS.attr('string'),
+  branch:               DS.attr('string'),
+  circuit_number:       DS.attr('string'),
+  MSAD_number:          DS.attr('string'),
+  activation_date:      DS.attr('date'),
+  MAC_address:          DS.attr('string'),
+  distance:             DS.attr('string'),
+  solution_identifier:  DS.attr('string'),
+  billing_account:      DS.attr('string'),
+  service_account:      DS.attr('string'),
+  service_account_site: DS.attr('string')
 });
 
 export default ClientLink;
