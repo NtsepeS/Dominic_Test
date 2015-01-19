f4fcbc6e2bc4d750d23666d5197da90acbf4379f
Ridhwana <Ridhwana.khan16@gmail.com>
2015-01-19 16:08:34 +0200

    refactor: follow ember conventions when using the active model adapter

--- ember/app/models/client-link.js
+++ ember/app/models/client-link.js
@@ -1,18 +1,18 @@
 import DS from 'ember-data';
 
 var ClientLink = DS.Model.extend({
-  name:                 DS.attr('string'),
-  branch:               DS.attr('string'),
-  circuit_number:       DS.attr('string'),
-  MSAD_number:          DS.attr('string'),
-  activation_date:      DS.attr('date'),
-  MAC_address:          DS.attr('string'),
-  distance:             DS.attr('string'),
-  solution_identifier:  DS.attr('string'),
-  billing_account:      DS.attr('string'),
-  service_account:      DS.attr('string'),
-  service_account_site: DS.attr('string'),
-  link_type:            DS.belongsTo('link-type')
+  name:               DS.attr('string'),
+  branch:             DS.attr('string'),
+  circuitNumber:      DS.attr('string'),
+  msadNumber:         DS.attr('string'),
+  activationDate:     DS.attr('date'),
+  macAddress:         DS.attr('string'),
+  distance:           DS.attr('string'),
+  solutionIdentifier: DS.attr('string'),
+  billingAccount:     DS.attr('string'),
+  serviceAccount:     DS.attr('string'),
+  serviceAccountSite: DS.attr('string'),
+  linkType:           DS.belongsTo('link-type')
 });
 
 export default ClientLink;
