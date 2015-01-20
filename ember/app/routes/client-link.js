929c0b4861257a124ded03cc72e0525547ddc905
Ridhwana <Ridhwana.khan16@gmail.com>
2015-01-20 11:53:20 +0200

    style: fix indentation

--- ember/app/routes/client-link.js
+++ ember/app/routes/client-link.js
@@ -3,18 +3,17 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function(params) {
-       var self = this;
-       return new Ember.RSVP.Promise(function (resolve) {
-              new Ember.RSVP.hash({
-                  clientLink: self.store.find('client-link', params.clientLinkId),
-                  linkTypes: self.store.find('link-type'),
-              }).then(function (results) {
-                 resolve({
-                     clientLink: results.clientLink,
-                     linkTypes: results.linkTypes
-                 });
-              });
-        });
+    var self = this;
+    return new Ember.RSVP.Promise(function (resolve) {
+      new Ember.RSVP.hash({
+        clientLink: self.store.find('client-link', params.clientLinkId),
+        linkTypes: self.store.find('link-type'),
+      }).then(function (results) {
+        resolve({
+          clientLink: results.clientLink,
+          linkTypes: results.linkTypes
+         });
+      });
+    });
   }
-
 });
