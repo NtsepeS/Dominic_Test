58e69a89a74d346ef8f3b250ec3de18b3c1223d9
Ridhwana <Ridhwana.khan16@gmail.com>
2015-01-20 20:29:32 +0200

    refactor: removed unused attribute

--- rails/app/serializers/client_link_serializer.rb
+++ rails/app/serializers/client_link_serializer.rb
@@ -1,7 +1,7 @@
 class ClientLinkSerializer < ActiveModel::Serializer
   attributes :id, :name, :branch, :circuit_number, :mac_address,
     :msad_number, :activation_date, :distance, :client, :status,
-    :network_operator, :base_station_sector, :antenna, :solution_identifier,
+    :network_operator, :base_station_sector, :solution_identifier,
     :billing_account, :service_account, :service_account_site, :created_at, :updated_at
 
   embed :ids, include: true
