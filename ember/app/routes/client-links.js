4a5087190e146aa4d54297fcb7503436b4c98166
Ridhwana <Ridhwana.khan16@gmail.com>
2015-01-21 16:13:30 +0200

    fix: remove the client links route as we now have an index route

--- ember/app/routes/client-links.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
-
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model: function(){
-    console.log(this.store.findAll('client_link'));
-    return this.store.findAll('client_link');
-  }
-});
