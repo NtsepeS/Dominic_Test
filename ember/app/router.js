d66ed5e97bb8cca075403c8292a68752298eb507
Ridhwana <Ridhwana.khan16@gmail.com>
2015-01-22 11:32:40 +0200

    feat/WIP: prepare the frontend for the audit history Create a route and consume the model from a possible url. Display the data via a template

--- ember/app/router.js
+++ ember/app/router.js
@@ -7,12 +7,12 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('login');
-  this.route('statuses');
   this.resource('client-links', function(){
     this.route('new');
   });
   this.resource('client-link', { path: 'client-links/:clientLinkId' }, function(){
-    this.route('edit')
+    this.route('edit');
+    this.route('audit');
   });
 });
 
---
+++ ember/app/routes/client-link/audit.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model: function(params) {
+    var clientLinkModel = this.modelFor('client-link');
+    var modelId         =  clientLinkModel.id
+    return Ember.$.getJSON("/api/v1/client_links/"+ modelId + "/history");
+  },
+  setupController: function(controller, model) {
+    controller.set('content', model.client_links);
+  }
+});
