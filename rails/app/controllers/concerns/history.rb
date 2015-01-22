ab5dc7313c81b6a7aca96af09b546cc92aefed57
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-01-22 15:57:12 +0200

    style: fix up whitespace and tabs

--- rails/app/controllers/concerns/history.rb
+++ rails/app/controllers/concerns/history.rb
@@ -1,26 +1,26 @@
 module History
   extend ActiveSupport::Concern
 
-    def papertrail_for( model )
-      model.versions.map do |v|
-        {
-          item_type: v.item_type,
-          id:        v.id,
-          event:     v.event,
-          person:    v.whodunnit,
-          date:      v.created_at,
-          changes:   serialize_changes(v.changeset)
-        }
-      end
+  def papertrail_for( model )
+    model.versions.map do |v|
+      {
+        item_type: v.item_type,
+        id:        v.id,
+        event:     v.event,
+        person:    v.whodunnit,
+        date:      v.created_at,
+        changes:   serialize_changes(v.changeset)
+      }
     end
+  end
 
-    def serialize_changes(changeset)
-      changeset.map do |changes|
-        {
-          field_name: changes[0],
-          from:       changes[1][0],
-          to:         changes[1][1],
-        }
-      end
+  def serialize_changes(changeset)
+    changeset.map do |changes|
+      {
+        field_name: changes[0],
+        from:       changes[1][0],
+        to:         changes[1][1],
+      }
     end
+  end
 end
