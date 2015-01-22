e285d5a8a2142ebbf2a42332003351db89ea263b
Ridhwana <Ridhwana.khan16@gmail.com>
2015-01-22 12:55:15 +0200

    fix: change the url for consuming the audit on the api

--- ember/app/routes/client-link/audit.js
+++ ember/app/routes/client-link/audit.js
@@ -5,7 +5,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function() {
     var clientLinkModel = this.modelFor('client-link');
     var modelId         =  clientLinkModel.id;
-    return Ember.$.getJSON("/api/v1/client_links/"+ modelId + "/history");
+    return Ember.$.getJSON("/api/v1/client_links/"+ modelId + "/audit");
   },
   setupController: function(controller, model) {
     controller.set('content', model.client_links);
