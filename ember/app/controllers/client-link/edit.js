7b3f1a209f5122dd38e3085ed10569d76437f81e
Ridhwana <Ridhwana.khan16@gmail.com>
2015-01-22 20:46:28 +0200

    feat: ensure the transitioning of routes once we save or delete

--- ember/app/controllers/client-link/edit.js
+++ ember/app/controllers/client-link/edit.js
@@ -3,12 +3,19 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   actions: {
     acceptChanges: function() {
-      this.get('model').save();
+      var self = this;
+      var editedClientLink = self.get('model');
+      editedClientLink.save().then(function(result){
+        self.transitionToRoute('client-link', result.id);
+      });
     },
     removeClientLink: function(){
+      var self = this;
       var clientLink = this.get('model');
       clientLink.deleteRecord();
-      clientLink.save();
+      clientLink.save().then(function(){
+        self.transitionToRoute('client-links');
+      });
     }
   }
 });
