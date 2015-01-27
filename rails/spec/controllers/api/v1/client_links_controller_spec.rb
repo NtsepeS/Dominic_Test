249d574a7cac83871318ac29d6fa070891227517
Ridhwana <Ridhwana.khan16@gmail.com>
2015-01-27 09:41:19 +0200

    test: show action test for the client_link

--- rails/spec/controllers/api/v1/client_links_controller_spec.rb
+++ rails/spec/controllers/api/v1/client_links_controller_spec.rb
@@ -32,4 +32,50 @@ RSpec.describe Api::V1::ClientLinksController do
 
   end
 
+  describe '#show' do
+    context 'for a valid id' do
+
+      let(:client_link) {create(:client_link)}
+
+      before do
+        get :show, id: client_link.id
+        @data = JSON.parse(response.body)
+      end
+
+      it 'returns http ok status' do
+        expect(response).to be_ok
+      end
+
+      it 'returns an outer key of client_link' do
+        expect(@data).to have_key('client_link')
+      end
+
+      it 'returns a Hash of information' do
+        expect(@data['client_link']).to be_a(Hash)
+      end
+
+      it 'returns results containing the appropriate keys' do
+        expect(@data['client_link'].keys.map(&:to_sym)).to include(:name, :branch,
+          :circuit_number, :mac_address, :msad_number, :activation_date, :distance,
+          :solution_identifier, :billing_account, :service_account,
+          :service_account_site, :created_at, :updated_at, :client_id, :link_type_id,
+          :antenna_id, :network_operator_id, :base_station_sector_id, :status_id
+        )
+      end
+
+      # it 'returns the hash of foreign keys in the outer hash' do
+      #   @data['client_link'].keys.each do |key|
+      #     if key=~ /_id$/
+      #       puts common = key.partition('_').first
+      #       expect(@data.keys).to include(common)
+      #     end
+      #   end
+      # end
+
+    end
+
+    context 'for an invalid id' do
+    end
+  end
+
 end
