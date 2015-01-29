0bb90abd6099cc79ff3e3ea1a3f5a84fbe83fd9f
André-Marthin Helberg <helberg.andre@gmail.com>
2015-01-29 11:56:14 +0200

    Added dates to Geometry, Location, Vicinity serializers

--- rails/app/serializers/geometry_serializer.rb
+++ rails/app/serializers/geometry_serializer.rb
@@ -1,3 +1,3 @@
 class GeometrySerializer < ActiveModel::Serializer
-  attributes :id, :latitude, :longitude, :altitude
+  attributes :id, :latitude, :longitude, :altitude, :created_at, :updated_at
 end
