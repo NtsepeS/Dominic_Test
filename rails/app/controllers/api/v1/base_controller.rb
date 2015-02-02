c712750516c8686450a64ba4b11bdc745c0bc601
Alecia Bester <alecia.bester@gmail.com>
2015-02-02 12:55:49 +0200

    Added not found rescue hook

--- rails/app/controllers/api/v1/base_controller.rb
+++ rails/app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,16 @@
+require 'pry'
 module Api
   module V1
     class BaseController < ApplicationController
+      rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+      protected
+
+      def render_404(exception)
+        #TODO: log exception message
+        puts exception.message
+        render status: :not_found, text: "not found"
+      end
     end
   end
 end
