4a0220441028ff82ec92b6aef92bfdf39cec2473
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-02-11 13:16:28 +0200

    feat: add extra attributes to equipment model

--- rails/app/serializers/equipment_serializer.rb
+++ rails/app/serializers/equipment_serializer.rb
@@ -1,3 +1,4 @@
 class EquipmentSerializer < ActiveModel::Serializer
-  attributes :id, :name, :is_asset_tag, :equipped_id, :equipped_type
+  attributes :id, :name, :is_asset_tag, :equipped_id, :equipped_type,
+    :serial_number, :model_number, :product_number
 end
--- rails/db/migrate/20150210085349_create_equipment.rb
+++ rails/db/migrate/20150210085349_create_equipment.rb
@@ -3,6 +3,9 @@ class CreateEquipment < ActiveRecord::Migration
     create_table :equipment do |t|
       t.string :name
       t.string :is_asset_tag
+      t.string :serial_number
+      t.string :model_number
+      t.string :product_number
       t.references :equipped, polymorphic: true, index: true
 
       t.timestamps null: false
