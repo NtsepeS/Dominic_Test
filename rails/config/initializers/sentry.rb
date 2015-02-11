1f4cdf4124e3ead6ca3ab96cdde6342d2fd85067
Alecia Bester <alecia.bester@gmail.com>
2015-02-11 11:17:06 +0200

    Set dsn to false for development env

--- rails/config/initializers/sentry.rb
+++ rails/config/initializers/sentry.rb
@@ -1,5 +1,3 @@
-require 'raven'
-
 Raven.configure do |config|
-  config.dsn = 'http://e099c1845a3f469d897ac86b6ddb5289:055d077012f5431cbe8c0e35f680a344@sentry.si.is.co.za/17'
+  config.dsn = Rails.application.secrets.sentry_dsn
 end
