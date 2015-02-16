5b750a4be5ee9b5b14907965df4421990b108963
Ridhwana <Ridhwana.khan16@gmail.com>
2015-02-16 11:42:53 +0200

    quick commit to revert once i've installed my ssd

--- ember/app/components/image-upload.js
+++ ember/app/components/image-upload.js
@@ -5,7 +5,16 @@ export default Ember.Component.extend({
     Em.assert('should have an url', this.get('url'));
     return this.$('#imageupload').fileupload({
       formData: {client_link_id: this.id},
-      url: this.get('url')
+      url: this.get('url'),
+      progressall: function (e, data) {
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        $('#progress .bar').css('width', progress + '%');
+        $('#progress .bar').text(progress + '%');
+      },
+      done: function(e, data){
+        // debugger
+        console.log(data.result)
+      }
     })
   },
 
