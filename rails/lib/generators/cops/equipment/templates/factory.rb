8dd6a3a76e46230ce4428e66ef3df4180974dcfe
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-02-19 19:30:30 +0200

    fix: formatting for factory template with more than one attribute

--- rails/lib/generators/cops/equipment/templates/factory.rb
+++ rails/lib/generators/cops/equipment/templates/factory.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :<%= singular_name %> do
-  <% attributes.each do |attr| -%>
-  <% unless attr.type == :references-%>
-<%= attr.name %> "<%= factory_type(attr.type)-%>"
-<% end -%>
-<% end -%>
-end
+    <%- attributes.each do |attr| -%>
+      <%- unless attr.type == :references -%>
+    <%= attr.name %> "<%= factory_type(attr.type)-%>"
+      <%- end -%>
+    <%- end -%>
+  end
 end
