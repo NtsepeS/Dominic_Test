59f25ccef2ed3e094a81de9a0f3527ace7f8b1b7
Alecia Bester <alecia.bester@gmail.com>
2015-02-19 12:52:30 +0200

    Linked Service Fragment to Client Link

--- rails/app/models/service_fragment.rb
+++ rails/app/models/service_fragment.rb
@@ -1,3 +1,4 @@
 class ServiceFragment < ActiveRecord::Base
   has_many :service
+  belongs_to :client_link
 end
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219063530) do
+ActiveRecord::Schema.define(version: 20150219104732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -230,6 +230,7 @@ ActiveRecord::Schema.define(version: 20150219063530) do
     t.string   "physical_mode"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "client_link_id"
   end
 
   create_table "services", force: :cascade do |t|
