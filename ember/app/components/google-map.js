7f6c6e5f0112883f4e228be21b5ee8b523257e06
Andre-Marthin Helberg <helberg.andre@gmail.com>
2015-02-24 09:36:58 +0200

    Added colour coding to core-nodes status on map

--- ember/app/components/google-map.js
+++ ember/app/components/google-map.js
@@ -13,10 +13,32 @@ export default Ember.Component.extend({
 
     var map = new google.maps.Map(container[0], options);
 
+    var decomed_image = 'assets/images/icon-outage.svg',
+        active_image =  'assets/images/icon-cleared.svg',
+        commissioning_image = 'assets/images/icon-threshold.svg';
+
     coreNodes.forEach(function(coreNode) {
+      var image = null;
+
+      switch (coreNode.get('status')) {
+        case "active":
+          image = active_image;
+          break;
+        case "decommissioned":
+          image = decomed_image;
+          break;
+        case "comissioning":
+          image = commissioning_image;
+          break;
+        default:
+          image = decomed_image;
+          break;
+        }
+
       var marker = new google.maps.Marker({
         position: new google.maps.LatLng(coreNode.get('latitude'), coreNode.get('longitude')),
         map: map,
+        icon: image,
         title: coreNode.get('name')
       });
     });
--- ember/app/routes/maps/core-nodes.js
+++ ember/app/routes/maps/core-nodes.js
@@ -4,9 +4,9 @@ export default Ember.Route.extend({
   model: function() {
     return {
       locations: [
-        Ember.Object.create({ name: 'Vanderbijlpark', latitude: -26.7217916, longitude: 27.8487259 }),
-        Ember.Object.create({ name: 'Pretoria', latitude: -25.7586499 , longitude: 28.219682 }),
-        Ember.Object.create({ name: 'Johannesburg', latitude: -26.2145598, longitude: 27.964365 })
+        Ember.Object.create({ name: 'Vanderbijlpark', latitude: -26.7217916, longitude: 27.8487259, status: "active" }),
+        Ember.Object.create({ name: 'Pretoria', latitude: -25.7586499 , longitude: 28.219682, status: "decommissioned" }),
+        Ember.Object.create({ name: 'Johannesburg', latitude: -26.2145598, longitude: 27.964365, status: "comissioning" })
       ]
     };
   }
