1089cb6ac48e52b8925aacd4760d3a921952d062
Alecia Bester <alecia.bester@gmail.com>
2015-02-24 09:16:02 +0200

    Display markers from stub locations

--- ember/app/components/google-map.js
+++ ember/app/components/google-map.js
@@ -2,15 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   insertMap: function() {
-    var container = this.$(".map-canvas");
+    var container = this.$(".map-canvas"),
+        coreNodes = this.get("coreNodes");
 
     var options = {
-      center: new google.maps.LatLng(this.get("latitude"),
-      this.get("longitude")),
-      zoom: 17,
+      center: new google.maps.LatLng(-26.1229025, 28.029034,12),
+      zoom: 6,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
-    new google.maps.Map(container[0], options);
+    var map = new google.maps.Map(container[0], options);
+
+    coreNodes.forEach(function(coreNode) {
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(coreNode.get('latitude'), coreNode.get('longitude')),
+        map: map,
+        title: coreNode.get('name')
+      });
+    });
   }.on('didInsertElement')
 });
--- ember/app/routes/maps/core-nodes.js
+++ ember/app/routes/maps/core-nodes.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return {
+      locations: [
+        Ember.Object.create({ name: 'Vanderbijlpark', latitude: -26.7217916, longitude: 27.8487259 }),
+        Ember.Object.create({ name: 'Pretoria', latitude: -25.7586499 , longitude: 28.219682 }),
+        Ember.Object.create({ name: 'Johannesburg', latitude: -26.2145598, longitude: 27.964365 })
+      ]
+    };
+  }
 });
