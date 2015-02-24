9835bb82d784ac186712ceae1c05f65e9739adea
Alecia Bester <alecia.bester@gmail.com>
2015-02-24 12:53:31 +0200

    Fixed relationships from ClientLinks to CoreNode

--- ember/app/models/base-station-unit.js
+++ ember/app/models/base-station-unit.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  city: DS.attr('string'),
   status: DS.belongsTo('status', { async: true }),
-  baseStationUnit: DS.hasMany('base-station-unit', {async: true})
+  baseStationSector: DS.hasMany('base-station-sector', { async: true }),
+  coreNode: DS.belongsTo('core-node', { async: true })
 });
