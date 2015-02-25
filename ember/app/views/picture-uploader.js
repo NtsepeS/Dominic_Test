2f574bae934ec2161d7a595d65dc99ddc118e3e6
Ridhwana <Ridhwana.khan16@gmail.com>
2015-02-25 22:20:14 +0200

    feat: once the view is inserted initialize the fileupload plugin, also make sure that it will be destroyed

--- ember/app/views/picture-uploader.js
+++ ember/app/views/picture-uploader.js
@@ -1,5 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  templateName: 'picture-uploader'
+  templateName: 'picture-uploader',
+
+  didInsertElement: function() {
+    var _this = this;
+    _this.url = '/api/v1/pictures';
+
+    Ember.assert('should have an url', _this.get('url'));
+
+    return _this.$('.js-picture-upload').fileupload({
+      url: _this.get('url')
+    });
+  },
+
+  willDestroyElement: function() {
+    return this.$('.js-picture-upload').fileupload('destroy');
+  }
+
 });
