3f880d62d02abf0e40aeb0f8cce47ba717857e7e
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-02-25 12:09:52 +0200

    fix: add missing sub_group_picture_set association

--- rails/app/models/album.rb
+++ rails/app/models/album.rb
@@ -1,3 +1,4 @@
 class Album < ActiveRecord::Base
   has_many :pictures
+  has_one :sub_group_picture_set
 end
--- rails/app/models/sub_group_classification.rb
+++ rails/app/models/sub_group_classification.rb
@@ -1,3 +1,4 @@
 class SubGroupClassification < ActiveRecord::Base
   belongs_to :group_classification
+  has_one :sub_group_picture_set
 end
