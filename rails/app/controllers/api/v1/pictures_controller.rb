64933973b8b3b5c9fd180c007dd1b37cf6b0703a
Ridhwana <Ridhwana.khan16@gmail.com>
2015-02-25 21:54:02 +0200

    feat: create action for the pictures controller -> able to upload a pic from postman

--- rails/app/controllers/api/v1/pictures_controller.rb
+++ rails/app/controllers/api/v1/pictures_controller.rb
@@ -24,6 +24,16 @@ module Api
         render json: picture
       end
 
+      def create
+
+        picture = Picture.new(picture_params)
+        if picture.save
+          render json: picture, status: :created
+        else
+          render json: picture.errors.to_json, status: :unprocessable_entity
+        end
+      end
+
       private
 
       def picture_params
