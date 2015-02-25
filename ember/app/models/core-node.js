171773b6e5d7c87b261935142674a41c9a067d20
Alecia Bester <alecia.bester@gmail.com>
2015-02-25 08:39:57 +0200

    Added location to ember core-node model

--- ember/app/models/core-node.js
+++ ember/app/models/core-node.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   city: DS.attr('string'),
   status: DS.belongsTo('status', { async: true }),
-  baseStationUnit: DS.hasMany('base-station-unit', {async: true})
+  baseStationUnit: DS.hasMany('base-station-unit', {async: true}),
+  location: DS.belongsTo('location', {async: true})
 });
