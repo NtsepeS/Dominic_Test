cbef888a4f5ef5eccc2c682ab88fe36e62987a1e
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-02-26 13:54:41 +0200

    feat: basic plumbing for running cucumber outside of our projects
    Based on http://dvg.github.io/2014/11/26/full-stack-integration-testing-with-rails-ember-and-cucumber.html

---
+++ cukes/config/cukes.rb
@@ -0,0 +1,16 @@
+class Cukes
+  require 'active_support/configurable'
+  include ActiveSupport::Configurable
+
+  self.configure do |config|
+    config.root = Dir[File.dirname(File.expand_path('../', __FILE__))].first
+    config.rails_root = File.join(config.root, "..", "rails")
+    config.rails_started_message = "Listening on tcp://"
+    config.ember_root = File.join(config.root, "..", "ember")
+    config.ember_started_message = "Build successful"
+    config.host = "http://localhost:4200"
+    config.browser = ENV["BROWSER"] || :phantomjs
+    config.startup_timeout = 45
+  end
+
+end
---
+++ cukes/features/support/application_manager.rb
@@ -0,0 +1,77 @@
+module ChildProcess
+  module Unix
+    class Process < AbstractProcess
+      def interrupt
+        send_signal "SIGINT"
+      end
+    end
+  end
+end
+
+class ApplicationManager
+  require 'childprocess'
+  attr_accessor :rails, :ember, :rails_log, :ember_log
+
+  def initialize
+    @rails = ChildProcess.build("sh", "-c", "BUNDLE_GEMFILE=Gemfile bundle exec rails s -e test")
+    @rails.leader = true
+    @rails.cwd = Cukes.config.rails_root
+    @rails_log = @rails.io.stdout = @rails.io.stderr = Tempfile.new('rails-log')
+
+    @ember = ChildProcess.build("ember", "serve", "--proxy", "http://localhost:3000")
+    @ember.leader = true
+    @ember.cwd = Cukes.config.ember_root
+    @ember_log = @ember.io.stdout = @ember.io.stderr = Tempfile.new("ember-log")
+  end
+
+  def start_stack
+    puts "Bringing the Applications Online, sit tight"
+    rails.start
+    ember.start
+    wait_for_processes_started
+    puts "Applications Online - Happy Cuking"
+  end
+
+  def stop_stack
+    puts "Stopping the Applications, hang on"
+    rails.interrupt
+    ember.interrupt
+    wait_for_processes_to_exit
+    puts "All done! Hope they're all passes (::) (::) (::)"
+  end
+
+
+private
+
+  def wait_for_processes_started
+    begin
+      Timeout::timeout(Cukes.config.startup_timeout) do
+        loop do
+           break if processes_started?
+        end
+      end
+    rescue Timeout::Error => e
+      rails.interrupt
+      ember.interrupt
+      wait_for_processes_to_exit
+      raise "Unable to start the application"
+    end
+  end
+
+  def wait_for_processes_to_exit
+    begin
+      Timeout::timeout(5) do
+        loop { break if rails.exited? && ember.exited? }
+      end
+    rescue Errno::ESRCH => e
+      # Already stopped the process, no biggie
+    rescue Timeout::Error => e
+      raise "Unable to exit processes. pids: #{rails.pid}, #{ember.pid}"
+    end
+  end
+
+  def processes_started?
+    open(rails_log).read.include?( Cukes.config.rails_started_message ) &&
+      open(ember_log).read.include?( Cukes.config.ember_started_message )
+  end
+end
