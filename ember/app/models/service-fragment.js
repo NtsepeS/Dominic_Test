73e437cab4bd7001c68aa7ed87916da99b94a0b5
Alecia Bester <alecia.bester@gmail.com>
2015-03-03 14:03:33 +0200

    Added missing service type attribute

--- ember/app/models/service-fragment.js
+++ ember/app/models/service-fragment.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   workOrderNumber: DS.attr('string'),
   lineSpeed:       DS.attr('number'),
   physicalMode:    DS.attr('string'),
+  serviceType:     DS.attr('string'),
   services:        DS.hasMany('service', {async: true}),
   clientLink:      DS.belongsTo('client-link', { async: true })
 });
