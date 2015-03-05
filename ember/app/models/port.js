6b4bc4e17c43aa0d79a500148e6183fc581429a3
Alecia Bester <alecia.bester@gmail.com>
2015-03-05 16:59:25 +0200

    Show collection of vlans

--- ember/app/models/port.js
+++ ember/app/models/port.js
@@ -4,5 +4,16 @@ export default DS.Model.extend({
   vlanType:            DS.attr('string'),
   acceptableFrameType: DS.attr('string'),
   defaultVlan:         DS.attr('string'),
-  service:             DS.belongsTo('service', {async: true})
+  service:             DS.belongsTo('service', {async: true}),
+
+  serviceFragmentVlans: function() {
+    var vlans = [];
+
+    var services = this.get('service.serviceFragment.services');
+    services.forEach( function(service) {
+      vlans.push(service.get('vlan'));
+    });
+
+    return vlans.join(", ");
+  }.property('service')
 });
