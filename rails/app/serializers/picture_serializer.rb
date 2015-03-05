3dbbd42500d4e42a5b7f9b4a320f4cf52f6c3132
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-05 19:28:40 +0200

    feat: set the width and height propertiesa and let it reflect through the api

--- rails/app/serializers/picture_serializer.rb
+++ rails/app/serializers/picture_serializer.rb
@@ -1,6 +1,6 @@
 class PictureSerializer < ActiveModel::Serializer
   attributes :id, :mechanism, :date_taken,
-    :album_id, :created_at, :updated_at, :url
+    :album_id, :created_at, :updated_at, :url, :width, :height
 
   def url
     object.picture_data.url
