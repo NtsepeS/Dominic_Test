221c9bf796f33a95d7712273f79587392eb9718e
Alecia Bester <alecia.bester@gmail.com>
2015-03-05 16:13:21 +0200

    Edit ports template and delete ports

--- ember/app/controllers/client-link/edit.js
+++ ember/app/controllers/client-link/edit.js
@@ -4,6 +4,8 @@ export default Ember.ObjectController.extend({
   lineSpeeds: [0.25, 0.5, 0.75, 1, 1.5, 2, 3, 4 ,5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 18 ,20, 25, 30, 35, 40, 45, 50],
   serviceTypes: ['Voice (rtPS)', 'Data 1:1 (nrtPS)', 'Data 1:2 (BE Plus)', 'Broadband (BE)'],
   physicalModes: ['4QAM2/3', '4QAM', '16QAM', '64QAM'],
+  vlanTypes: ['Provider Port', 'VLAN Transparent'],
+  acceptableFrameTypes: ['Tagged Only', 'Untagged Only', 'Tagged and Untagged'],
   addServiceFragment: false,
   actions: {
     acceptChanges: function() {
@@ -43,6 +45,11 @@ export default Ember.ObjectController.extend({
     removeService: function(serviceFragment, service) {
       serviceFragment.get('services').removeObject(service);
       service.destroyRecord();
+    },
+
+    removePort: function(service, port) {
+      service.get('ports').removeObject(port);
+      port.destroyRecord();
     },
 
     addServiceFragment: function() {
