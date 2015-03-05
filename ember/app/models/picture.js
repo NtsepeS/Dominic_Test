0b6798ea7e511805c5462875d18d218253f5d4b4
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-05 19:29:11 +0200

    feat: add the widtha nd height to the model on the frontend

--- ember/app/models/picture.js
+++ ember/app/models/picture.js
@@ -4,6 +4,8 @@ var Pictures = DS.Model.extend({
   mechanism: DS.attr('string'),
   url:       DS.attr('string'),
   dateTaken: DS.attr('date'),
+  width:     DS.attr('string'),
+  height:    DS.attr('string'),
   album:     DS.belongsTo('album', {async: true})
 });
 
