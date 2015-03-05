b6c7af06df6d9104fc69e1ab405f30bc87b1ac10
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-05 12:36:56 +0200

    refactor: rename self to _this

--- ember/app/controllers/client-link/edit.js
+++ ember/app/controllers/client-link/edit.js
@@ -3,18 +3,18 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   actions: {
     acceptChanges: function() {
-      var self = this;
-      var editedClientLink = self.get('model');
+      var _this = this;
+      var editedClientLink = _this.get('model');
       editedClientLink.save().then(function(result){
-        self.transitionToRoute('client-link', result.id);
+        _this.transitionToRoute('client-link', result.id);
       });
     },
     removeClientLink: function(){
-      var self = this;
+      var _this = this;
       var clientLink = this.get('model');
       clientLink.deleteRecord();
       clientLink.save().then(function(){
-        self.transitionToRoute('client-links');
+        _this.transitionToRoute('client-links');
       });
     },
     pictureUploaded: function(data) {
