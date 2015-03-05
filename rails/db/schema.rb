d16bdf2e8e916b769fe4a1143e1e65aa3b39e1a2
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-05 19:25:22 +0200

    feat: add width and height columns to the picture table

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219104732) do
+ActiveRecord::Schema.define(version: 20150305144322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -226,6 +226,8 @@ ActiveRecord::Schema.define(version: 20150219104732) do
     t.integer  "album_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "width"
+    t.string   "height"
   end
 
   add_index "pictures", ["album_id"], name: "index_pictures_on_album_id", using: :btree
