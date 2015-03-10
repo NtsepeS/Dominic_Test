89e8e6d57ee8521ec1ea7804db4995dcca68ab77
Alecia Bester <alecia.bester@gmail.com>
2015-03-10 17:23:46 +0200

    Properties observes the controller directly

--- ember/app/views/maps/view.js
+++ ember/app/views/maps/view.js
@@ -1,49 +1,39 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  filterOperational: Ember.computed.alias('controller.filterOperational'),
-  filterUnderConstruction: Ember.computed.alias('controller.filterUnderConstruction'),
-  filterDecommissioned: Ember.computed.alias('controller.filterDecommissioned'),
-  filterCommissioning: Ember.computed.alias('controller.filterCommissioning'),
-  filterToBeDecommissioned: Ember.computed.alias('controller.filterToBeDecommissioned'),
-  filterWaitingForFieldServiceEngineerReport: Ember.computed.alias('controller.filterWaitingForFieldServiceEngineerReport'),
-  filterWaitingForFieldServicesQAApproval: Ember.computed.alias('controller.filterWaitingForFieldServicesQAApproval'),
-  filterWaitingForISQAApproval: Ember.computed.alias('controller.filterWaitingForISQAApproval'),
-  filterClientLinks: Ember.computed.alias('controller.filterClientLinks'),
-
   toggleOperational: function() {
     $('.operational').toggleClass('disabled');
-  }.observes('filterOperational'),
+  }.observes('controller.filterOperational'),
 
   toggleUnderConstruction: function() {
     $('.under-construction').toggleClass('disabled');
-  }.observes('filterUnderConstruction'),
+  }.observes('controller.filterUnderConstruction'),
 
   toggleDecommissioned: function() {
     $('.decommissioned').toggleClass('disabled');
-  }.observes('filterDecommissioned'),
+  }.observes('controller.filterDecommissioned'),
 
   toggleCommissioning: function() {
     $('.commissioning').toggleClass('disabled');
-  }.observes('filterCommissioning'),
+  }.observes('controller.filterCommissioning'),
 
   toggleToBeDecommissioned: function() {
     $('.to-be-decommissioned').toggleClass('disabled');
-  }.observes('filterToBeDecommissioned'),
+  }.observes('controller.filterToBeDecommissioned'),
 
   toggleWaitingForFieldServiceEngineerReport: function() {
     $('.waiting-for-field-service-engineer-report').toggleClass('disabled');
-  }.observes('filterWaitingForFieldServiceEngineerReport'),
+  }.observes('controller.filterWaitingForFieldServiceEngineerReport'),
 
   toggleWaitingForFieldServicesQAApproval: function() {
     $('.waiting-for-field-services-qa-approval').toggleClass('disabled');
-  }.observes('filterWaitingForFieldServicesQAApproval'),
+  }.observes('controller.filterWaitingForFieldServicesQAApproval'),
 
   toggleWaitingForISQAApproval: function() {
     $('.waiting-for-is-qa-approval').toggleClass('disabled');
-  }.observes('filterWaitingForISQAApproval'),
+  }.observes('controller.filterWaitingForISQAApproval'),
 
   toggleClientLinks: function() {
     $('.client-link').toggleClass('disabled');
-  }.observes('filterClientLinks')
+  }.observes('controller.filterClientLinks')
 });
