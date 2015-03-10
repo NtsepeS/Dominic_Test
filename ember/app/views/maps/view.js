d949f8c40a8ece164363aa5e2ddf2890d51a8e30
Alecia Bester <alecia.bester@gmail.com>
2015-03-10 17:35:26 +0200

    Fixed JQuery JSHint

--- ember/app/views/maps/view.js
+++ ember/app/views/maps/view.js
@@ -2,38 +2,38 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   toggleOperational: function() {
-    $('.operational').toggleClass('disabled');
+    Ember.$('.operational').toggleClass('disabled');
   }.observes('controller.filterOperational'),
 
   toggleUnderConstruction: function() {
-    $('.under-construction').toggleClass('disabled');
+    Ember.$('.under-construction').toggleClass('disabled');
   }.observes('controller.filterUnderConstruction'),
 
   toggleDecommissioned: function() {
-    $('.decommissioned').toggleClass('disabled');
+    Ember.$('.decommissioned').toggleClass('disabled');
   }.observes('controller.filterDecommissioned'),
 
   toggleCommissioning: function() {
-    $('.commissioning').toggleClass('disabled');
+    Ember.$('.commissioning').toggleClass('disabled');
   }.observes('controller.filterCommissioning'),
 
   toggleToBeDecommissioned: function() {
-    $('.to-be-decommissioned').toggleClass('disabled');
+    Ember.$('.to-be-decommissioned').toggleClass('disabled');
   }.observes('controller.filterToBeDecommissioned'),
 
   toggleWaitingForFieldServiceEngineerReport: function() {
-    $('.waiting-for-field-service-engineer-report').toggleClass('disabled');
+    Ember.$('.waiting-for-field-service-engineer-report').toggleClass('disabled');
   }.observes('controller.filterWaitingForFieldServiceEngineerReport'),
 
   toggleWaitingForFieldServicesQAApproval: function() {
-    $('.waiting-for-field-services-qa-approval').toggleClass('disabled');
+    Ember.$('.waiting-for-field-services-qa-approval').toggleClass('disabled');
   }.observes('controller.filterWaitingForFieldServicesQAApproval'),
 
   toggleWaitingForISQAApproval: function() {
-    $('.waiting-for-is-qa-approval').toggleClass('disabled');
+    Ember.$('.waiting-for-is-qa-approval').toggleClass('disabled');
   }.observes('controller.filterWaitingForISQAApproval'),
 
   toggleClientLinks: function() {
-    $('.client-link').toggleClass('disabled');
+    Ember.$('.client-link').toggleClass('disabled');
   }.observes('controller.filterClientLinks')
 });
