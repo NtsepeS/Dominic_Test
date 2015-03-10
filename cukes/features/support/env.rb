afdea6a4a350a88b750d5c77d4454f4acd519c6f
Pieter van der Merwe <vandermerwe.pieter@gmail.com>
2015-03-10 09:59:37 +0200

    Suppress paper_trails deprecation warnings

--- cukes/features/support/env.rb
+++ cukes/features/support/env.rb
@@ -51,3 +51,10 @@ end
 
 # Shorthand FactoryGirl
 include FactoryGirl::Syntax::Methods
+
+# Silence paper_trail deprecation warning
+current_behavior = ActiveSupport::Deprecation.behavior
+ActiveSupport::Deprecation.behavior = lambda do |message, callstack|
+  return if message =~ /`serialized_attributes` is deprecated without replacement/ && callstack.any? { |m| m =~ /paper_trail/ }
+  Array.wrap(current_behavior).each { |behavior| behavior.call(message, callstack) }
+end
