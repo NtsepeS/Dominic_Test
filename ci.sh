67595a69722a349c233ae732ce54e981c1805423
Pieter van der Merwe <vandermerwe.pieter@gmail.com>
2015-03-10 14:26:18 +0200

    Add cucumber tests to ci

--- ci.sh
+++ ci.sh
@@ -4,10 +4,22 @@ rvm use "2.2.0"
 # Set this for after rvm is done loading, to keep CI output low
 set -x
 
+# rails tests
+
 cd rails
 
 export RAILS_ENV=test
-
 bundle install
 bundle exec rake db:drop db:create db:migrate db:test:prepare
 bundle exec rake ci:setup:rspec spec
+
+cd ..
+
+# cucumber tests
+
+cd cukes
+
+bundle install
+cucumber --format json -o cucumber.json
+
+cd ..
