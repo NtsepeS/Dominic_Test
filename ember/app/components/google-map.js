0da3d301c6444e1cce4056b94aff649bdf840dbb
Alecia Bester <alecia.bester@gmail.com>
2015-03-10 12:49:02 +0200

    Removed display concerns back to component

--- ember/app/components/google-map.js
+++ ember/app/components/google-map.js
@@ -57,7 +57,7 @@ export default Ember.Component.extend({
       var path = new google.maps.Polyline({
         path: clienLinkCoordinates,
         geodesic: true,
-        strokeColor: clientLink.get('status.getColour'),
+        strokeColor: this.getColour(clientLink.get('status')),
         strokeOpacity: 0.5,
         strokeWeight: 2
       });
@@ -107,11 +107,45 @@ export default Ember.Component.extend({
 
   getImage: function(node, isCoreNode) {
     if (isCoreNode) {
-      return node.get('status.getCoreNodeStatusImage');
+      return this.getCoreNodeStatusImage(node.get('status'));
     }
     else {
-      return node.get('status.getStatusImage');
+      return this.getStatusImage(node.get('status'));
     }
+  },
+
+  getColour: function(statusName) {
+    return {
+      "Operational": '#649131',
+      "Under Construction": '#EEA506',
+      "Decommissioned": '#D8DDE1',
+      "Commissioning": '#3D96EF',
+      "To Be Decommissioned": '#A50077',
+      "Waiting for Field Service Engineer Report": '#DD3E00',
+      "Waiting for Field Services QA Approval": '#310031',
+      "Waiting for IS QA Approval": '#D2493C'
+    }[statusName] || '#D8DDE1';
+  },
+
+  getStatusImage: function(statusName) {
+    return {
+      "Operational": 'assets/images/operational.svg',
+      "Under Construction": 'assets/images/under-construction.svg',
+      "Decommissioned": 'assets/images/decommissioned.svg',
+      "Commissioning": 'assets/images/commissioning.svg',
+      "To Be Decommissioned": 'assets/images/to-be-decommissioned.svg',
+      "Waiting for Field Service Engineer Report": 'assets/images/waiting-for-field-service-engineer-report.svg',
+      "Waiting for Field Services QA Approval": 'assets/images/waiting-for-field-services-qa-approval.svg',
+      "Waiting for IS QA Approval": 'assets/images/waiting-for-is-qa-approval.svg'
+    }[statusName] || 'assets/images/decommissioned.svg';
+  },
+
+  getCoreNodeStatusImage: function(statusName) {
+    return {
+      "Operational": 'assets/images/core_node_operational.svg',
+      "Under Construction": 'assets/images/core_node_under_construction.svg',
+      "Decommissioned": 'assets/images/core_node_decommissioned.svg'
+    }[statusName] || 'assets/images/core_node_decommissioned.svg';
   },
 
   willDestroyElement: function() {
--- ember/app/models/status.js
+++ ember/app/models/status.js
@@ -1,39 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string'),
-
-  getColour: function() {
-    return {
-      "Operational": '#649131',
-      "Under Construction": '#EEA506',
-      "Decommissioned": '#D8DDE1',
-      "Commissioning": '#3D96EF',
-      "To Be Decommissioned": '#A50077',
-      "Waiting for Field Service Engineer Report": '#DD3E00',
-      "Waiting for Field Services QA Approval": '#310031',
-      "Waiting for IS QA Approval": '#D2493C'
-    }[this.get('name')] || '#D8DDE1';
-  }.property('name'),
-
-  getStatusImage: function() {
-    return {
-      "Operational": 'assets/images/operational.svg',
-      "Under Construction": 'assets/images/under-construction.svg',
-      "Decommissioned": 'assets/images/decommissioned.svg',
-      "Commissioning": 'assets/images/commissioning.svg',
-      "To Be Decommissioned": 'assets/images/to-be-decommissioned.svg',
-      "Waiting for Field Service Engineer Report": 'assets/images/waiting-for-field-service-engineer-report.svg',
-      "Waiting for Field Services QA Approval": 'assets/images/waiting-for-field-services-qa-approval.svg',
-      "Waiting for IS QA Approval": 'assets/images/waiting-for-is-qa-approval.svg'
-    }[this.get('name')] || 'assets/images/decommissioned.svg';
-  }.property('name'),
-
-  getCoreNodeStatusImage: function() {
-    return {
-      "Operational": 'assets/images/core_node_operational.svg',
-      "Under Construction": 'assets/images/core_node_under_construction.svg',
-      "Decommissioned": 'assets/images/core_node_decommissioned.svg'
-    }[this.get('name')] || 'assets/images/core_node_decommissioned.svg';
-  }.property('name')
+  name: DS.attr('string')
 });
