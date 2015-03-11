272ae29b02408fa251117aab583b8a69f711d24f
Pieter van der Merwe <vandermerwe.pieter@gmail.com>
2015-03-11 15:43:44 +0200

    Extra large order of debug statements for CI please.

--- cukes/features/support/env.application_manager.rb
+++ cukes/features/support/env.application_manager.rb
@@ -31,7 +31,9 @@ class ApplicationManager
   def start_stack
     puts "Bringing the Applications Online, sit tight"
     rails.start
+    puts "start_stack1"
     ember.start
+    puts "start_stack2"
     wait_for_processes_started
     puts "Applications Online - Happy Cuking"
   end
@@ -58,35 +60,57 @@ private
 
   def wait_for_processes_started
     begin
+      puts "wait_for_processes_started 1"
       Timeout::timeout(Cukes.config.startup_timeout) do
+        puts "wait_for_processes_started 2"
         loop do
+           puts "wait_for_processes_started 3"
            break if processes_started?
+           puts "wait_for_processes_started 4"
         end
+        puts "wait_for_processes_started 5"
       end
+      puts "wait_for_processes_started 6"
     rescue Timeout::Error => e
+      puts "wait_for_processes_started 7"
       dump_logs
+      puts "wait_for_processes_started 8"
       rails.interrupt
+      puts "wait_for_processes_started 9"
       ember.interrupt
+      puts "wait_for_processes_started 10"
       wait_for_processes_to_exit
+      puts "wait_for_processes_started 11"
       raise "Unable to start the application"
     end
+    puts "wait_for_processes_started 12"
     dump_logs
+    puts "wait_for_processes_started 13"
   end
 
   def wait_for_processes_to_exit
     begin
+      puts "wait_for_processes_to_exit 1"
       Timeout::timeout(5) do
+        puts "wait_for_processes_to_exit 2"
         loop { break if rails.exited? && ember.exited? }
+        puts "wait_for_processes_to_exit 3"
       end
+      puts "wait_for_processes_to_exit 4"
     rescue Errno::ESRCH => e
+      puts "wait_for_processes_to_exit 5"
       # Already stopped the process, no biggie
     rescue Timeout::Error => e
+      puts "wait_for_processes_to_exit 6"
       raise "Unable to exit processes. pids: #{rails.pid}, #{ember.pid}"
     end
+    puts "wait_for_processes_to_exit 7"
   end
 
   def processes_started?
+    puts "processes_started 1"
     sleep 15
+    puts "processes_started 2"
     true 
     # open(rails_log).read.include?( Cukes.config.rails_started_message ) &&
       # open(ember_log).read.include?( Cukes.config.ember_started_message )
