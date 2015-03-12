b16b768ebcc6f91bbd84d105d1286210fb33daa0
Alecia Bester <alecia.bester@gmail.com>
2015-03-12 13:16:15 +0200

    Refactored line-speeds into own model

--- ember/app/controllers/service-fragments/edit.js
+++ ember/app/controllers/service-fragments/edit.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
+import LineSpeeds from 'cops/models/line-speeds';
 
 export default Ember.ObjectController.extend({
-  lineSpeeds: [0.25, 0.5, 0.75, 1, 1.5, 2, 3, 4 ,5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 18 ,20, 25, 30, 35, 40, 45, 50],
+  lineSpeeds: LineSpeeds,
   serviceTypes: ['Voice (rtPS)', 'Data 1:1 (nrtPS)', 'Data 1:2 (BE Plus)', 'Broadband (BE)'],
   physicalModes: ['4QAM2/3', '4QAM', '16QAM', '64QAM'],
   actions: {
