f8ad6ffbc4c8bbe112a367a5cbed06f3088ac9de
Alecia Bester <alecia.bester@gmail.com>
2015-03-13 08:26:24 +0200

    Refactored delete-prompt to own component

---
+++ ember/app/components/prompt-modal.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+import layout from '../templates/components/prompt-modal';
+
+export default Ember.Component.extend({
+  layout: layout,
+  actions: {
+    deleteItem: function() {
+      this.sendAction();
+    }
+  }
+});
