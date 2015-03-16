479ddb17829d09234140df27427f4ce7861b263b
Alecia Bester <alecia.bester@gmail.com>
2015-03-16 08:54:23 +0200

    Make use of mixins to remove duplicate functionality of map-filters for different views

--- ember/app/views/maps/view.js
+++ ember/app/views/maps/view.js
@@ -1,39 +1,4 @@
 import Ember from 'ember';
+import MapFilters from 'cops/mixins/map-filters';
 
-export default Ember.View.extend({
-  toggleOperational: function() {
-    Ember.$('.operational').toggleClass('disabled');
-  }.observes('controller.filterOperational'),
-
-  toggleUnderConstruction: function() {
-    Ember.$('.under-construction').toggleClass('disabled');
-  }.observes('controller.filterUnderConstruction'),
-
-  toggleDecommissioned: function() {
-    Ember.$('.decommissioned').toggleClass('disabled');
-  }.observes('controller.filterDecommissioned'),
-
-  toggleCommissioning: function() {
-    Ember.$('.commissioning').toggleClass('disabled');
-  }.observes('controller.filterCommissioning'),
-
-  toggleToBeDecommissioned: function() {
-    Ember.$('.to-be-decommissioned').toggleClass('disabled');
-  }.observes('controller.filterToBeDecommissioned'),
-
-  toggleWaitingForFieldServiceEngineerReport: function() {
-    Ember.$('.waiting-for-field-service-engineer-report').toggleClass('disabled');
-  }.observes('controller.filterWaitingForFieldServiceEngineerReport'),
-
-  toggleWaitingForFieldServicesQAApproval: function() {
-    Ember.$('.waiting-for-field-services-qa-approval').toggleClass('disabled');
-  }.observes('controller.filterWaitingForFieldServicesQAApproval'),
-
-  toggleWaitingForISQAApproval: function() {
-    Ember.$('.waiting-for-is-qa-approval').toggleClass('disabled');
-  }.observes('controller.filterWaitingForISQAApproval'),
-
-  toggleClientLinks: function() {
-    Ember.$('.client-link').toggleClass('disabled');
-  }.observes('controller.filterClientLinks')
-});
+export default Ember.View.extend(MapFilters,{});
