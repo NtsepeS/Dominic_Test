51d2eb477405eed33340f8fa391221e2ec69ed0c
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-03-17 19:44:57 +0200

    feat: add ability to walk tree from base station sector

--- rails/app/models/base_station_sector.rb
+++ rails/app/models/base_station_sector.rb
@@ -3,5 +3,13 @@ class BaseStationSector < ActiveRecord::Base
 
   has_one :container, as: :containable
 
+  delegate :parent, :children, to: :container
+  delegate :base_station_unit, to: :parent
+  delegate :core_node, to: :base_station_unit
+
   belongs_to :status
+
+  def client_links
+    children.map(&:client_link)
+  end
 end
