7e99af11f4f69d73b08c0ba8f012d32f599bc3e8
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-03-17 17:41:49 +0200

    feat: add tests for core_node model tree traversal

---
+++ rails/spec/models/core_node_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe CoreNode, :type => :model do
+
+  # core node
+  let (:c1) { Container.new }
+  let (:cn) { CoreNode.new name: "Bryanston Towers", container: c1 }
+
+  # base station unit
+  let (:c2) { c1.children.new }
+  let (:bs1) { BaseStationUnit.new name: "Station 2", container: c2 }
+
+  let (:c3) { c1.children.new }
+  let (:bs2) { BaseStationUnit.new name: "Station 4", container: c3 }
+
+  # base station sector
+  let (:c4) { c2.children.new }
+  let (:sec) { BaseStationSector.new name: "Sector 3", container: c4 }
+
+  # client link
+  let (:c6) { c4.children.new }
+  let (:cl) { ClientLink.new name: "Client Link 1", container: c6 }
+
+  it 'has a container linked to it' do
+    expect(cn.container).to eq(c1)
+  end
+
+  context 'responds to its methods' do
+    it { expect(cn).to respond_to(:children) }
+    it { expect(cn).to respond_to(:base_station_units) }
+    it { expect(cn).to respond_to(:base_station_sectors) }
+    it { expect(cn).to respond_to(:client_links) }
+  end
+
+  context 'executes methods correctly' do
+    it 'will delegate children method call to attached container' do
+      expect(cn.children).to eq(c1.children)
+    end
+
+    it 'will return a list of base station units' do
+      expect(cn.base_station_units).to include(bs1, bs2)
+    end
+
+    it 'will return a list of base station sectors' do
+      expect(cn.base_station_sectors).to include(sec)
+    end
+
+    it 'will return a list of client links' do
+      expect(cn.client_links).to include(cl)
+    end
+  end
+end
