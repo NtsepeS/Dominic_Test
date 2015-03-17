0e28abfebbd1c0c86da9687731a5367ec5c62c4c
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-03-17 12:57:15 +0200

    style: rename hasMany associations to be plural

--- ember/app/models/base-station-unit.js
+++ ember/app/models/base-station-unit.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string'),
-  status: DS.belongsTo('status', { async: true }),
-  baseStationSector: DS.hasMany('base-station-sector', { async: true }),
-  coreNode: DS.belongsTo('core-node', { async: true })
+  name:               DS.attr('string'),
+  status:             DS.belongsTo('status', { async: true }),
+  baseStationSectors: DS.hasMany('base-station-sector', { async: true }),
+  coreNode:           DS.belongsTo('core-node', { async: true })
 });
--- ember/app/models/core-node.js
+++ ember/app/models/core-node.js
@@ -1,27 +1,27 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string'),
-  city: DS.attr('string'),
-  status: DS.belongsTo('status', { async: true }),
-  baseStationUnit: DS.hasMany('base-station-unit', {async: true}),
-  location: DS.belongsTo('location', {async: true}),
+  name:              DS.attr('string'),
+  city:              DS.attr('string'),
+  status:            DS.belongsTo('status', { async:          true }),
+  baseStationUnits:  DS.hasMany('base-station-unit', {async:  true}),
+  location:          DS.belongsTo('location', {async:         true}),
 
   clientLinks: function() {
     // This returns a flattened list of clientlinks for this core-node
     var clientLinks = [],
-        baseStationUnits = this.get('baseStationUnit');
+        baseStationUnits = this.get('baseStationUnits');
 
     baseStationUnits.forEach( function(baseStationUnit){
-      baseStationUnit.get('baseStationSector').forEach( function(baseStationSector) {
-        baseStationSector.get('clientLink').forEach(function (clientLink){
+      baseStationUnit.get('baseStationSectors').forEach( function(baseStationSector) {
+        baseStationSector.get('clientLinks').forEach(function (clientLink){
           clientLinks.push(clientLink);
         });
       });
     });
 
     return clientLinks;
-  }.property('baseStationUnit'),
+  }.property('baseStationUnits'),
 
   latitude: function() {
     return this.get('location.geometry.latitude');
