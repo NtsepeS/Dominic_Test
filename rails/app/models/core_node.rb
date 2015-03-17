34ac6a1342029a346bd87a767a8792a3d9e2d953
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-03-17 18:50:49 +0200

    feat: add ability to walk down tree from core node

--- rails/app/models/core_node.rb
+++ rails/app/models/core_node.rb
@@ -3,7 +3,21 @@ class CoreNode < ActiveRecord::Base
 
   has_one :container, as: :containable
 
+  delegate :children, to: :container
+
   belongs_to :status
   belongs_to :city
   belongs_to :location
+
+  def base_station_units
+    children.map(&:base_station_unit)
+  end
+
+  def base_station_sectors
+    base_station_units.map(&:base_station_sectors).flatten
+  end
+
+  def client_links
+    base_station_sectors.map(&:client_links).flatten
+  end
 end
