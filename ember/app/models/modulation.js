19bf1ce7ed960ef171b9ca46dbbfe45874e0a1e3
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-17 23:33:02 +0200

    feat: update the client-link, radio and modulation models on the frintend

--- ember/app/models/modulation.js
+++ ember/app/models/modulation.js
@@ -1,8 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  uplinkMax:         DS.attr('number'),
-  downlinkMax:       DS.attr('number'),
-  uplinkMin:         DS.attr('number'),
-  downlinkMin:       DS.attr('number'),
+  uplinkMax:   DS.attr('number'),
+  downlinkMax: DS.attr('number'),
+  uplinkMin:   DS.attr('number'),
+  downlinkMin: DS.attr('number'),
+  clientLink:  DS.belongsTo('client-link', {async: true}),
 });
--- ember/app/models/radio.js
+++ ember/app/models/radio.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name:         DS.attr('string'),
   itemCode:     DS.attr('string'),
-  icasaSticker: DS.attr('string')
+  icasaSticker: DS.attr('string'),
+  modulations:  DS.hasMany('modulation', {async: true}),
 });
