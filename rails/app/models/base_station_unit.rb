4065de57defeec7f4df341a40c4d2da384e4a049
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-03-17 19:16:46 +0200

    feat: add ability to walk up & down tree from base station unit

--- rails/app/models/base_station_unit.rb
+++ rails/app/models/base_station_unit.rb
@@ -3,5 +3,12 @@ class BaseStationUnit < ActiveRecord::Base
 
   has_one :container, as: :containable
 
+  delegate :parent, :children, to: :container
+  delegate :core_node, to: :parent
+
   belongs_to :status
+
+  def base_station_sectors
+    children.map(&:base_station_sector)
+  end
 end
