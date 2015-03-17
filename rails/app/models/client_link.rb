1c1dc08fd3f532a920addd664f2998ac0278a8ef
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-03-17 20:05:24 +0200

    feat: add ability to walk up tree from client link

--- rails/app/models/client_link.rb
+++ rails/app/models/client_link.rb
@@ -3,11 +3,16 @@ class ClientLink < ActiveRecord::Base
 
   has_one :container, as: :containable
 
+  delegate :parent, :children, to: :container
+  delegate :base_station_sector, to: :parent
+  delegate :base_station_unit, to: :base_station_sector
+  delegate :core_node, to: :base_station_unit
+
   belongs_to :client
   belongs_to :status
   belongs_to :network_operator
   belongs_to :link_type
-  has_many   :service_fragment
 
+  has_many :service_fragment
   has_many :sub_group_picture_sets
 end
