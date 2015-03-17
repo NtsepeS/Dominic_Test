707d4db83e2d4f7a762f1ddb3333190aa814bbdc
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-03-17 13:12:09 +0200

    feat: add a method_missing to container so tree is easier to navigate

--- rails/app/models/container.rb
+++ rails/app/models/container.rb
@@ -2,4 +2,12 @@ class Container < ActiveRecord::Base
   acts_as_nested_set
 
   belongs_to :containable, polymorphic: true
+
+  def method_missing(method, *args, &block)
+    if self.containerizable_type.underscore == method.to_s
+      self.containerizable( *args, &block )
+    else
+      super
+    end
+  end
 end
