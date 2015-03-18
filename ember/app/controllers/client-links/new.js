266efe32a73d9f0bcddbb064e74339745cca4936
Alecia Bester <alecia.bester@gmail.com>
2015-03-18 12:13:45 +0200

    Fixed create new client link

--- ember/app/controllers/client-links/new.js
+++ ember/app/controllers/client-links/new.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   actions: {
-    acceptChanges: function() {
-      var _this         = this,
-          newClientLink = this.get('model');
-      newClientLink.save().then(function(result){
-        _this.transitionToRoute('client-link', result.id);
+    updateOverview: function() {
+      var _this         = this;
+
+      this.get('model').save().then(function(result){
+        _this.transitionToRoute('client-link.overview.index', result.id);
       });
     }
   }
