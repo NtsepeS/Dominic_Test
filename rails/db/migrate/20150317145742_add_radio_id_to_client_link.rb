56b662922d1aedf896e7d7534f683b0146a00542
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-18 16:50:01 +0200

    fix: migrations for adding the radio_id and index to the client link

--- rails/db/migrate/20150317145742_add_radio_id_to_client_link.rb
+++
@@ -1,5 +0,0 @@
-class AddRadioIdToClientLink < ActiveRecord::Migration
-  def change
-    add_column :client_links, :radio_id, :integer
-  end
-end
--- rails/db/migrate/20150317172652_add_index_to_radio_id_to_client_links.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToRadioIdToClientLinks < ActiveRecord::Migration
-  def change
-    add_index :client_links, :radio_id
-  end
-end
