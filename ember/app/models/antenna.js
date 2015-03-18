ed6d7e32c82a0f1c1de154e32a0101d6da614b5a
Alecia Bester <alecia.bester@gmail.com>
2015-03-18 14:23:12 +0200

    Added missing association

--- ember/app/models/antenna.js
+++ ember/app/models/antenna.js
@@ -4,4 +4,5 @@ export default DS.Model.extend({
   size:      DS.attr('string'),
   location:  DS.belongsTo('location', {async: true}),
   item_code: DS.attr('string'),
+  clientLinks: DS.hasMany('client-link', {async: true})
 });
