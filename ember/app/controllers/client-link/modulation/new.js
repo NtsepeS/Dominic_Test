b15dbd5c664b0fd40f110376c5025291eae6207d
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-19 15:20:26 +0200

    WIP: create a radio and save modulation values on a new template

--- ember/app/controllers/client-link/modulation/new.js
+++ ember/app/controllers/client-link/modulation/new.js
@@ -4,4 +4,46 @@ import ModulationValues from 'cops/models/modulation-values';
 export default Ember.ObjectController.extend({
   needs: "client-link",
   modulationValues: ModulationValues,
+  actions: {
+    saveModulation: function() {
+      var _this = this
+      var radio = this.store.createRecord('radio', {});
+
+      radio.get('modulations').addObject(this.store.createRecord('modulation',{
+        downlinkMin: this.get('selectedPredictedDLMin'),
+        downlinkMax: this.get('selectedPredictedDLMax'),
+        uplinkMin:   this.get('selectedPredictedULMin'),
+        uplinkMax:   this.get('selectedPredictedULMax'),
+      }));
+
+      radio.get('modulations').addObject(this.store.createRecord('modulation',{
+        downlinkMin: this.get('selectedConfiguredDLMin'),
+        downlinkMax: this.get('selectedConfiguredDLMax'),
+        uplinkMin:   this.get('selectedConfiguredULMin'),
+        uplinkMax:   this.get('selectedConfiguredULMax'),
+      }));
+
+      radio.save().then(function(){
+        var promises = Ember.A();
+        radio.get('modulations').forEach(function(item){
+          promises.push(item.save());
+        _this.get('radio');
+        _this.set('radio', radio)
+        _this.get('model').save().then(
+          console.log("after save", _this.get('radio'))
+        )
+        });
+
+        Ember.RSVP.Promise.all(promises).then(function(resolvedPromises){
+          _this.transitionTo('modulation.index');
+          alert("All Saved")
+        }).catch(function () {
+          console.log('one of the saves failed');
+        });
+      });
+
+    }
+  }
+
+
 });
