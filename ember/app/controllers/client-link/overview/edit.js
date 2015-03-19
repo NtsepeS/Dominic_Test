0a0868f759d1798227877a981a2e9687614ca317
Alecia Bester <alecia.bester@gmail.com>
2015-03-19 10:15:58 +0200

    Refactored controller to use RSVP promises

--- ember/app/controllers/client-link/overview/edit.js
+++ ember/app/controllers/client-link/overview/edit.js
@@ -1,57 +1,56 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-  actions: {
-    updateOverview: function() {
-      var _this = this;
+  setupPromise: function(modelProperty, model) {
+    var _this   = this,
+        keyName = 'model.' + modelProperty + '.id';
 
-// (WIP!!!!!!!!!)
-// This mess is because when saving assocaited models the parent models doesn't persist
-// https://github.com/emberjs/data/issues/437#issuecomment-9782817
-// https://github.com/emberjs/data/pull/724
-// http://discuss.emberjs.com/t/saving-multiple-hasmany/4907
-
-      this.store.find('client', this.get('model.client.id')).then(function(client) {
-        client.save().then(function(client) {
-          _this.get('model').set('client',client);
-
-          _this.store.find('link-type', _this.get('model.linkType.id')).then(function(linkType) {
-            linkType.save().then(function(linkType) {
-              _this.get('model').set('linkType', linkType);
-
-              _this.store.find('antenna', _this.get('model.antenna.id')).then(function(antenna) {
-                antenna.save().then(function(antenna) {
-                  _this.get('model').set('antenna', antenna);
-
-                  _this.store.find('network-operator', _this.get('model.networkOperator.id')).then(function(networkOperator) {
-                    networkOperator.save().then(function(networkOperator) {
-                      _this.get('model').set('networkOperator', networkOperator);
-
-                      _this.store.find('base-station-sector', _this.get('model.baseStationSector.id')).then(function(baseStationSector) {
-                        baseStationSector.save().then(function(baseStationSector) {
-                          _this.get('model').set('baseStationSector', baseStationSector);
-
-                          _this.store.find('status', _this.get('model.status.id')).then(function(status) {
-                          status.save().then(function(status) {
-                            _this.get('model').set('status', status);
-
-                            _this.get('model').save().then(function() {
-                               _this.transitionToRoute('client-link.overview');
-                            });
+    if (model === undefined) {
+      model = modelProperty;
+    }
 
 
-                          });
-                        });
-                      });
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      if (_this.get(keyName) === undefined ){
+        return resolve(undefined);
+      }
+
+      var storeModelPromise = _this.store.find(model, _this.get(keyName));
+      storeModelPromise.then(function(storeModel) {
+        return resolve(storeModel.save());
       });
     });
+  },
+
+  actions: {
+    updateOverview: function() {
+      var _this                    = this,
+          clientPromise            = this.setupPromise('client'),
+          linkTypePromise          = this.setupPromise('linkType', 'link-type'),
+          antennaPromise           = this.setupPromise('antenna'),
+          networkOperatorPromise   = this.setupPromise('networkOperator', 'network-operator'),
+          baseStationSectorPromise = this.setupPromise('baseStationSector', 'base-station-sector'),
+          statusPromise            = this.setupPromise('status');
+
+      Promise.all([
+        clientPromise,
+        linkTypePromise,
+        antennaPromise,
+        networkOperatorPromise,
+        baseStationSectorPromise,
+        statusPromise
+      ]).then(function(values){
+        _this.get('model').set('client', values[0]);
+        _this.get('model').set('linkType',values[1]);
+        _this.get('model').set('antenna',values[2]);
+        _this.get('model').set('networkOperator',values[3]);
+        _this.get('model').set('baseStationSector',values[4]);
+        _this.get('model').set('status',values[5]);
+
+        _this.get('model').save().then(function() {
+           _this.transitionToRoute('client-link.overview');
+        });
+      });
     }
   }
 });
