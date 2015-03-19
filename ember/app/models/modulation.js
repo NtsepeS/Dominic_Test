132b07d020114ce994e273128283a5b494ac4c17
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-19 17:14:44 +0200

    feat: update the modulation model on the frontend to reflect the backend model

--- ember/app/models/modulation.js
+++ ember/app/models/modulation.js
@@ -1,9 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  uplinkMax:   DS.attr('string'),
-  downlinkMax: DS.attr('string'),
-  uplinkMin:   DS.attr('string'),
-  downlinkMin: DS.attr('string'),
-  radio:       DS.belongsTo('radio', {async: true}),
+  uplinkMax:           DS.attr('string'),
+  downlinkMax:         DS.attr('string'),
+  uplinkMin:           DS.attr('string'),
+  downlinkMin:         DS.attr('string'),
+  modulationResultSet: DS.attr('string'),
+  radio:               DS.belongsTo('radio', {async: true}),
 });
