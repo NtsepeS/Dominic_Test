90315942159e022808c6655e84fd5f26d74384ad
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-19 16:56:18 +0200

    feat: ability to edit and save

--- ember/app/controllers/client-link/modulation/edit.js
+++ ember/app/controllers/client-link/modulation/edit.js
@@ -4,4 +4,11 @@ import ModulationValues from 'cops/models/modulation-values';
 export default Ember.ObjectController.extend({
   needs: "client-link",
   modulationValues: ModulationValues,
+  actions: {
+    saveModulation: function(){
+      this.get('model').save().then(
+        this.transitionToRoute('client-link.modulation')
+      );
+    }
+  }
 });
