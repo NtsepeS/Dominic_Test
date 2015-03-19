aa4b1e8faa04ea52e096b096ca1ecaf074fd883c
Alecia Bester <alecia.bester@gmail.com>
2015-03-19 11:08:25 +0200

    Create new client link fixed (with initial flash message)

--- ember/app/controllers/client-links/new.js
+++ ember/app/controllers/client-links/new.js
@@ -1,13 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  showInfo: false,
   actions: {
     updateOverview: function() {
       var _this         = this;
 
       this.get('model').save().then(function(result){
-        _this.transitionToRoute('client-link.overview.index', result.id);
+        _this.transitionToRoute('client-link.overview.edit', result.id);
       });
+    },
+
+    showInfo: function() {
+      this.set('showInfo', true);
     }
   }
 });
