8c3902d89234bc83abcd7eeda25c0b4f2b3b1d7f
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-19 15:16:50 +0200

    fix: icasaSticker match to boolean on the rails side

--- ember/app/models/radio.js
+++ ember/app/models/radio.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name:                    DS.attr('string'),
   itemCode:                DS.attr('string'),
-  icasaSticker:            DS.attr('string'),
+  icasaSticker:            DS.attr('boolean'),
   modulations:             DS.hasMany('modulation', {async: true}),
   rfPerformanceParameters: DS.hasMany('rf-performance-parameters', {async: true})
 });
