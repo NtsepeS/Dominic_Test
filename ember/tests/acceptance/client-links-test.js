99ebe5c75df66ea704b12792e78e5eb0210ec217
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-03-20 13:10:38 +0200

    style: applied BEM to client links table, updated tests too

--- ember/tests/acceptance/client-links-test.js
+++ ember/tests/acceptance/client-links-test.js
@@ -42,9 +42,9 @@ test('visiting /client-links should show a list of links', function(assert) {
   andThen(function() {
     assert.equal(currentPath(), 'client-links.index');
 
-    var links = find("table.js-client-links-table tbody tr");
+    var links = find(".c-client-link-list-item");
     assert.equal( links.length, 1 );
-    var name = links.find("td:eq(0)");
+    var name = links.find(".c-client-link-list-item__name");
     assert.equal( name.text(), "Upington" );
   });
 });
@@ -59,13 +59,14 @@ test('clicking on an entry in the client links table should navigate to the deta
   visit('/client-links');
 
   andThen(function() {
-    var name = find("table.js-client-links-table tbody td:eq(0)");
+    var name = find(".c-client-link-list-item__name:eq(0)");
     assert.equal( name.text(), "Upington" );
   });
-  click("table.js-client-links-table tbody tr:eq(0)");
+  click(".c-client-link-list-item:eq(0)");
   andThen(function(){
     assert.equal(currentPath(), 'client-link.index');
     var heading = find("#site_information h2");
     assert.equal( heading.text(), "Site Information" );
   });
 });
+
