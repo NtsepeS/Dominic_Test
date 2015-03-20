388f47a44a0b6349995e927fb54ad8ca82697f12
Alecia Bester <alecia.bester@gmail.com>
2015-03-20 10:47:03 +0200

    Make use of mixin to save client-links and associated models (can be exchanged with improved promise utils later) on different views.

--- ember/app/controllers/client-link/overview/edit.js
+++ ember/app/controllers/client-link/overview/edit.js
@@ -1,58 +1,17 @@
 import Ember from 'ember';
+import SaveClienLink from 'cops/mixins/save-client-link';
 
-export default Ember.ObjectController.extend({
-  setupPromise: function(modelProperty, model) {
-    var _this   = this,
-        keyName = 'model.' + modelProperty + '.id';
-
-    if (model === undefined) {
-      model = modelProperty;
-    }
-
-    return new Promise(function(resolve) {
-      // Resolve if empty value for promise chain to continue
-      if (_this.get(keyName) === undefined ){
-        return resolve(undefined);
-      }
-
-      // Resolve after save on associated model
-      var promise = _this.store.find(model, _this.get(keyName));
-
-      promise.then(function(storeModel) {
-        return resolve(storeModel.save());
-      });
+export default Ember.ObjectController.extend(SaveClienLink, {
+  saveModel: function() {
+    var _this         = this;
+    _this.get('model').save().then(function(result) {
+      _this.transitionToRoute('client-link.overview', result.id);
     });
   },
 
   actions: {
     updateOverview: function() {
-      var _this                    = this,
-          clientPromise            = this.setupPromise('client'),
-          linkTypePromise          = this.setupPromise('linkType', 'link-type'),
-          antennaPromise           = this.setupPromise('antenna'),
-          networkOperatorPromise   = this.setupPromise('networkOperator', 'network-operator'),
-          baseStationSectorPromise = this.setupPromise('baseStationSector', 'base-station-sector'),
-          statusPromise            = this.setupPromise('status');
-
-      var promise = Promise.all([
-        clientPromise,
-        linkTypePromise,
-        antennaPromise,
-        networkOperatorPromise,
-        baseStationSectorPromise,
-        statusPromise
-      ]).then(function(values){
-        _this.get('model').set('client', values[0]);
-        _this.get('model').set('linkType',values[1]);
-        _this.get('model').set('antenna',values[2]);
-        _this.get('model').set('networkOperator',values[3]);
-        _this.get('model').set('baseStationSector',values[4]);
-        _this.get('model').set('status',values[5]);
-
-        _this.get('model').save().then(function() {
-           _this.transitionToRoute('client-link.overview');
-        });
-      });
+      this.saveAssociated();
     }
   }
 });
--- ember/app/controllers/client-links/new.js
+++ ember/app/controllers/client-links/new.js
@@ -1,14 +1,19 @@
 import Ember from 'ember';
+import SaveClienLink from 'cops/mixins/save-client-link';
 
-export default Ember.ObjectController.extend({
+export default Ember.ObjectController.extend(SaveClienLink, {
   showInfo: false,
+
+  saveModel: function() {
+    var _this         = this;
+    this.get('model').save().then(function(result){
+      _this.transitionToRoute('client-link.overview.edit', result.id);
+    });
+  },
+
   actions: {
     updateOverview: function() {
-      var _this         = this;
-
-      this.get('model').save().then(function(result){
-        _this.transitionToRoute('client-link.overview.edit', result.id);
-      });
+      this.saveAssociated();
     },
 
     showInfo: function() {
