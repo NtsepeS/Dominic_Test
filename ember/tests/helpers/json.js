8d979cd43533a2baa36c4b96711194a5bbc53a83
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-03-20 12:55:56 +0200

    chore: document the things

--- ember/tests/helpers/json.js
+++ ember/tests/helpers/json.js
@@ -1,3 +1,15 @@
+/**
+ * A little test helper function for building up JSON response bodies
+ * from the provided object, for use with pretender.
+ *
+ * Eg:
+ *
+ *     server.get('/foo', json(200, { foo: "bar" })
+ *
+ * This will return a function that accepts the request, and returns
+ * a response object as expected by Pretender, with 200 status code,
+ * content-type headers set, and '{"foo":"bar"}' as the response body.
+ */
 export default function(statusCode, attrs) {
   return function(req){
     return [
