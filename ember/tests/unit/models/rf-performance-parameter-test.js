e33a810758eac4e6313a26ede559f20fb4f9a957
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-22 21:24:01 +0200

    tests: fix the model tests on the frontend to maintain relationships

--- ember/tests/unit/models/rf-performance-parameter-test.js
+++ ember/tests/unit/models/rf-performance-parameter-test.js
@@ -5,7 +5,7 @@ import {
 
 moduleForModel('rf-performance-parameter', 'RfPerformanceParameter', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:radio', 'model:modulation']
 });
 
 test('it exists', function() {
