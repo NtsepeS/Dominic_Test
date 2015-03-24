1b981ef8883a6733ea3ede0e6366d10ad7fd005f
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-24 08:45:06 +0200

    style: whitespace

--- ember/app/controllers/client-link/rf-performance-parameters/new.js
+++ ember/app/controllers/client-link/rf-performance-parameters/new.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-  needs:          "client-link",
+  needs: "client-link",
 
   actions: {
     saveRFPerformance: function() {
