1c44031e43aee6a2c1ad8737d622d40b2f118d15
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-03-24 16:38:59 +0200

    test: use new rspec expect syntax

--- rails/spec/models/service_fragment_spec.rb
+++ rails/spec/models/service_fragment_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe ServiceFragment do
   it "should be valid" do
-    ServiceFragment.new.should be_valid
+    expect(subject).to be_valid
   end
 end
--- rails/spec/models/service_spec.rb
+++ rails/spec/models/service_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe Service do
   it "should be valid" do
-    Service.new.should be_valid
+    expect(subject).to be_valid
   end
 end
