f3727ca3f5dee5d34b86160d104b71813b7d9463
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-24 09:31:49 +0200

    WIP: if a radio exists, use that radio id and add the modulations

--- ember/app/controllers/client-link/modulations/new.js
+++ ember/app/controllers/client-link/modulations/new.js
@@ -4,45 +4,88 @@ import ModulationValues from 'cops/models/modulation-values';
 export default Ember.ObjectController.extend({
   needs: "client-link",
   modulationValues: ModulationValues,
+
   actions: {
     saveModulation: function() {
       var _this = this;
-      var radio = this.store.createRecord('radio', {});
 
-      radio.get('modulations').addObject(this.store.createRecord('modulation',{
-        modulationResultSet: 'Predicted',
-        downlinkMin:         this.get('selectedPredictedDLMin'),
-        downlinkMax:         this.get('selectedPredictedDLMax'),
-        uplinkMin:           this.get('selectedPredictedULMin'),
-        uplinkMax:           this.get('selectedPredictedULMax'),
-      }));
+      var radio = _this.get('model.radio.id');
 
-      radio.get('modulations').addObject(this.store.createRecord('modulation',{
-        modulationResultSet: 'Configured',
-        downlinkMin:         this.get('selectedConfiguredDLMin'),
-        downlinkMax:         this.get('selectedConfiguredDLMax'),
-        uplinkMin:           this.get('selectedConfiguredULMin'),
-        uplinkMax:           this.get('selectedConfiguredULMax'),
-      }));
+      if(radio) {
+        console.log("WE HAVE AN EXISTING RADIO AND ADD Mod");
+        debugger
+        // var modulations = this.store.createRecord('modulation', {})
 
-      radio.save().then(function() {
-        var promises = Ember.A();
-        radio.get('modulations').forEach(function(item){
-          promises.push(item.save());
+        var predictedModulation = this.store.createRecord('modulation',{
+          modulationResultSet: 'Predicted',
+          downlinkMin:         this.get('selectedPredictedDLMin'),
+          downlinkMax:         this.get('selectedPredictedDLMax'),
+          uplinkMin:           this.get('selectedPredictedULMin'),
+          uplinkMax:           this.get('selectedPredictedULMax'),
+          radio:               _this.get('model.radio')
         });
-        _this.set('radio', radio);
 
-        Ember.RSVP.Promise.all(
-          [promises,
-          _this.get('model').save()]
-        )
-        .then(function() {
+        var configuredModulation = this.store.createRecord('modulation',{
+          modulationResultSet: 'Configured',
+          downlinkMin:         this.get('selectedConfiguredDLMin'),
+          downlinkMax:         this.get('selectedConfiguredDLMax'),
+          uplinkMin:           this.get('selectedConfiguredULMin'),
+          uplinkMax:           this.get('selectedConfiguredULMax'),
+          radio:               _this.get('model.radio')
+        });
+
+        Promise.all([
+          predictedModulation.save(),
+          configuredModulation.save()
+        ]).then(function(){
           _this.transitionToRoute('client-link.modulations');
-        })
-        .catch(function() {
+        }).catch(function() {
           console.log('one of the saves failed');
         });
-      });
+
+      }
+      else {
+        console.log("WE CREATE A RADIO, AND ADD Mod");
+
+        var radio = this.store.createRecord('radio', {});
+
+        radio.get('modulations').addObject(this.store.createRecord('modulation',{
+          modulationResultSet: 'Predicted',
+          downlinkMin:         this.get('selectedPredictedDLMin'),
+          downlinkMax:         this.get('selectedPredictedDLMax'),
+          uplinkMin:           this.get('selectedPredictedULMin'),
+          uplinkMax:           this.get('selectedPredictedULMax'),
+        }));
+
+        radio.get('modulations').addObject(this.store.createRecord('modulation',{
+          modulationResultSet: 'Configured',
+          downlinkMin:         this.get('selectedConfiguredDLMin'),
+          downlinkMax:         this.get('selectedConfiguredDLMax'),
+          uplinkMin:           this.get('selectedConfiguredULMin'),
+          uplinkMax:           this.get('selectedConfiguredULMax'),
+        }));
+
+        radio.save().then(function() {
+          var promises = Ember.A();
+          radio.get('modulations').forEach(function(item){
+            promises.push(item.save());
+          });
+          _this.set('radio', radio);
+
+          Ember.RSVP.Promise.all(
+            [promises,
+            _this.get('model').save()]
+          )
+          .then(function() {
+            _this.transitionToRoute('client-link.modulations');
+          })
+          .catch(function() {
+            console.log('one of the saves failed');
+          });
+        });
+      }
+
+
     }
   }
 
