6bbfabe7bc998e9d80f9b682485cede4886e3955
Andre <helberg.andre@gmail.com>
2015-03-24 09:11:30 +0200

    feat: Handle connection timeouts from backend to iced

--- ember/app/controllers/authorizations/new.js
+++ ember/app/controllers/authorizations/new.js
@@ -1,11 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  invite_error:false,
+  error: undefined,
   actions: {
     sendInvite: function() {
       var _this = this;
       this.get('model').save().then(function(){
         _this.transitionToRoute('admin.authorization');
+      }, function(value){
+        var errors = value.errors;
+        if (errors['network']){
+          _this.set('error',"The backend timed out please try again later.");
+        } else {
+          _this.set('error',"Please check your network connection and try again.");
+        }
+        _this.set('invite_error', true);
       });
     }
   }
