d4acbe3caf989179a686116f49d478a1a648d4c1
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-25 14:29:54 +0200

    feat: create a new radio

---
+++ ember/app/controllers/client-link/radio/index.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    saveRadio: function(){
+      var _this = this;
+      var radio = _this.store.createRecord('radio', {});
+
+      radio.save().then(function(){
+        var clientLink = _this.get('model');
+        clientLink.set('radio', radio);
+        clientLink.save()
+      });
+    }
+  }
+});
