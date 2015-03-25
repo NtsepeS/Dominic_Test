c6853defd7f3127d6b62276b564a0a036e25fa86
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-25 14:36:23 +0200

    refactor: no need to check for a radio id because we will ensure that you cannot create a n rf-param if  a radio does not exist

--- ember/app/controllers/client-link/rf-performance-parameters/new.js
+++ ember/app/controllers/client-link/rf-performance-parameters/new.js
@@ -11,47 +11,14 @@ export default Ember.Controller.extend({
       var rfResultSet;
       var radio = _this.get('model.radio.id');
 
-      if(radio) {
-        console.log("existing radio id, create new rf");
+      var rfPerformanceParameter = this.get('model');
+      rfPerformanceParameter.set('radio', _this.get('model.radio'));
 
-        var rfPerformanceParameter = this.get('model');
-        rfPerformanceParameter.set('radio', _this.get('model.radio'));
-
-        rfPerformanceParameter.save().then(function(){
-          _this.transitionToRoute('client-link.rf-performance-parameters');
-        }).catch(function() {
-          console.log('one of the saves failed');
-        });
-
-      }
-      else {
-        console.log("create radio id, create new rf");
-
-        var Promise = Ember.RSVP.Promise;
-
-        radio = _this.store.createRecord('radio', {});
-        radio.get('rfPerformanceParameters').addObject(_this.createRFPerformanceRecord());
-
-        radio.save().then(function() {
-          var promises = Ember.A();
-          radio.get('rfPerformanceParameters').forEach(function(item){
-            promises.push(item.save());
-          });
-          _this.set('radio', radio);
-
-          Promise.all([
-            promises,
-            _this.get('model').save()
-          ]).then(function() {
-            _this.transitionToRoute('client-link.rf-performance-parameters');
-            _this.resetProperties();
-          }).catch(function() {
-            console.log('one of the saves failed');
-          });
-
-        });
-      }
-
+      rfPerformanceParameter.save().then(function(){
+        _this.transitionToRoute('client-link.rf-performance-parameters');
+      }).catch(function() {
+        console.log('one of the saves failed');
+      });
     }
   }
 
