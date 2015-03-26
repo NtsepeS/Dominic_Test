8f6afccbc342aa7d7d09d670f90aea6aaf25cb3a
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-26 16:43:25 +0200

    feat: get the exif date from the image when uploaded else use created at

--- rails/app/uploaders/picture_uploader.rb
+++ rails/app/uploaders/picture_uploader.rb
@@ -16,6 +16,13 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  # Get the exif data from the uploaded file
+  def get_exif( name )
+    manipulate! do |img|
+      return img["EXIF:" + name]
+    end
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
