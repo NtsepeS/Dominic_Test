2ff6dd478bdae5062f0014fd44d83824f3e8f83c
Andre <helberg.andre@gmail.com>
2015-03-26 14:34:19 +0200

    No longer using radio and antenna directly through client link

--- rails/app/models/client_link.rb
+++ rails/app/models/client_link.rb
@@ -18,8 +18,6 @@ class ClientLink < ActiveRecord::Base
   belongs_to :network_operator
   belongs_to :link_type
   belongs_to :base_station_sector
-  belongs_to :antenna
-  belongs_to :radio
 
   has_many :service_fragment
   has_many :sub_group_picture_sets
