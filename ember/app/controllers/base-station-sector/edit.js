7a511b07cc329a3fc16733a5e20d13195cac0212
Alecia Bester <alecia.bester@gmail.com>
2015-03-26 08:05:21 +0200

    fix: Fixed action name

--- ember/app/controllers/base-station-sector/edit.js
+++ ember/app/controllers/base-station-sector/edit.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    saveBaseStationUnit: function() {
+    saveBaseStationSector: function() {
       var _this = this;
 
       Promise.all([
