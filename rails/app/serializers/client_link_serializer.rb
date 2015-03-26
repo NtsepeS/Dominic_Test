3f3850cc9190510319ca042285e072fac876b9e1
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-03-26 20:14:29 +0200

    fix: move antenna, modem & radio to attributes in serializer

--- rails/app/serializers/client_link_serializer.rb
+++ rails/app/serializers/client_link_serializer.rb
@@ -2,10 +2,9 @@ class ClientLinkSerializer < ActiveModel::Serializer
   attributes :id, :name, :branch, :circuit_number, :mac_address, :msad_number,
     :activation_date, :distance, :solution_identifier,
     :billing_account, :service_account, :service_account_site, :created_at,
-    :updated_at
+    :updated_at, :antenna, :modem, :radio
 
-  has_one :client, :link_type, :network_operator, :status,
-    :antenna, :modem, :radio
+  has_one :client, :link_type, :network_operator, :status
 
   has_many :sub_group_picture_sets
 end
