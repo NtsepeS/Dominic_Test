8d4d39a0d204c8115b4ce2c2552d01390a9e92a9
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-03-27 12:10:25 +0200

    feat: smarter defaults

--- rails/spec/support/container_factories.rb
+++ rails/spec/support/container_factories.rb
@@ -2,25 +2,25 @@
 # hierarchies
 module ContainerFactories
 
-  def create_client_link( factory_name, parent = nil )
+  def create_client_link( factory_name = :client_link, parent = nil )
     parent ||= create_base_station_sector( :base_station_sector )
     containable = FactoryGirl.build( factory_name )
     create_containable( containable, parent )
   end
 
-  def create_base_station_sector( factory_name, parent = nil )
+  def create_base_station_sector( factory_name = :base_station_sector, parent = nil )
     parent ||= create_base_station_unit( :base_station_unit )
     containable = FactoryGirl.build( factory_name )
     create_containable( containable, parent )
   end
 
-  def create_base_station_unit( factory_name, parent = nil )
+  def create_base_station_unit( factory_name = :base_station_unit, parent = nil )
     parent ||= create_core_node( :core_node )
     containable = FactoryGirl.build( factory_name )
     create_containable( containable, parent )
   end
 
-  def create_core_node( factory_name )
+  def create_core_node( factory_name = :core_node )
     containable = FactoryGirl.build( factory_name )
     create_containable( containable )
   end
