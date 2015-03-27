8620d7ecc4045a8add2581268c78220a708fef84
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-27 16:50:01 +0200

    test: to make radio index pass

--- ember/tests/unit/controllers/client-link/radio/index-test.js
+++ ember/tests/unit/controllers/client-link/radio/index-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('controller:client-link/radio/index', {
+moduleFor('controller:client-link/radio', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
