cd7aec8ad434d4330767074d83aa42d81f2001c6
Pieter van der Merwe <vandermerwe.pieter@gmail.com>
2015-03-27 07:41:34 +0200

    Remove debug output and formatting cleanup

--- rails/app/services/workflow_service.rb
+++ rails/app/services/workflow_service.rb
@@ -2,17 +2,15 @@ require "processmaker"
 
 class WorkflowService
 
-  DefaultRole = "PROCESSMAKER_OPERATOR"
-  UserDefaults = {role: DefaultRole}
+  UserDefaults = {role: 'PROCESSMAKER_OPERATOR' }
 
   def initialize
-    ap Rails.application.secrets.PM_WSDL
     Processmaker.login(wsdl: Rails.application.secrets.PM_WSDL,
                        user_id: Rails.application.secrets.PM_ADMIN_USER,
                        password: Rails.application.secrets.PM_ADMIN_PASSWORD)
   end
 
-  def add_user options = {}
+  def add_user(options = {})
     options.reverse_merge!(UserDefaults)
     Processmaker.create_user(options)
   end
