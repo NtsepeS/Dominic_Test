8e2c5424c045e774042adcbcc822a0c3664e003d
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-27 22:04:45 +0200

    refactor: update picture group models on the front-end

--- ember/app/models/sub-group-classification.js
+++ ember/app/models/sub-group-classification.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 var SubGroupClassification = DS.Model.extend({
   name:                DS.attr('string'),
   groupClassification: DS.belongsTo('group-classification', {async: true}),
-  subGroupPictureSets: DS.hasMany('sub-group-picture-set', {async: true})
+  albums:              DS.hasMany('album', {async: true})
 });
 
 export default SubGroupClassification;
