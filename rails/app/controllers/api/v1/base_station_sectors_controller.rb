e6376ba936c811e6342bba2f78581fb6d0465413
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-03-27 13:01:17 +0200

    feat: added missing controller specs and parent container params

--- rails/app/controllers/api/v1/base_station_sectors_controller.rb
+++ rails/app/controllers/api/v1/base_station_sectors_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class BaseStationSectorsController < AuthenticatedController
       include ExcelGenerator
+      include ParentContainerParams
 
       def index
         base_station_sectors = BaseStationSector.all
@@ -11,6 +12,19 @@ module Api
       def show
         base_station_sector = BaseStationSector.find(params[:id])
         render json: base_station_sector
+      end
+
+      def create
+        container = parent_container( :base_station_sector, :base_station_unit_id )
+
+        bss = BaseStationSector.new( base_station_sector_params )
+        ncs = NewContainerService.new.create( bss, in: container )
+
+        if ncs.successful?
+          render json: ncs.containable, status: :created
+        else
+          render json: ncs.errors.to_json, status: :unprocessable_entity
+        end
       end
 
       def update
@@ -35,7 +49,7 @@ module Api
       private
 
       def base_station_sector_params
-        params.require(:base_station_sector).permit(:name)
+        params.require(:base_station_sector).permit(:name, :status, :sector)
       end
 
     end
