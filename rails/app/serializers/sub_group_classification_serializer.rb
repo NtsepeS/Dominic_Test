9f92ce1deef83e39a4288bfde6243f92e8488afb
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-27 21:41:59 +0200

    refactor: update picture group serialisers

--- rails/app/serializers/sub_group_classification_serializer.rb
+++ rails/app/serializers/sub_group_classification_serializer.rb
@@ -1,5 +1,5 @@
 class SubGroupClassificationSerializer < ActiveModel::Serializer
   attributes :id, :name, :group_classification_id, :created_at, :updated_at
 
-  has_many :sub_group_picture_sets
+  has_many :albums
 end
