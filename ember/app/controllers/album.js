f7c106975d4c3b794fd10fbc7a5e3d86a3fc78bc
Ridhwana <Ridhwana.khan16@gmail.com>
2015-03-29 09:53:49 +0200

    chore: just to distinguish name for now

--- ember/app/controllers/album.js
+++ ember/app/controllers/album.js
@@ -4,12 +4,20 @@ export default Ember.Controller.extend({
   needs: ['client-link'],
 
 
-  pictures: function() {
+  picturez: function() {
     var clientLink = this.get('controllers.client-link'),
          albums = this.get('model.albums'),
          pictures = this.get('model.pictures')
          pictures = pictures.sortBy('dateTaken');
-    return pictures
+    return pictures;
   }.property('model.pictures.isFulfilled'),
 
+
+  pic: function() {
+    var clientLink = this.get('controllers.client-link'),
+       albums = this.get('model.albums'),
+     pictures = this.get('model.pictures')
+     pictures = pictures.sortBy('dateTaken')
+    return pictures.get('lastObject');
+  }.property('model.pictures.[]')
 });
