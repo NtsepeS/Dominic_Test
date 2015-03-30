9ae0442ad0da17c05d15377a5612d1a6af0c41d2
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-03-30 21:18:49 +0200

    feat: add create action to core node controller & update allowed params

--- rails/app/controllers/api/v1/core_nodes_controller.rb
+++ rails/app/controllers/api/v1/core_nodes_controller.rb
@@ -12,6 +12,19 @@ module Api
         render json: core_node
       end
 
+      def create
+        # don't need to use ParentContainerParams since it's the root
+
+        core_node = CoreNode.new(core_node_params)
+        ncs = NewContainerService.new.create(core_node)
+
+        if ncs.successful?
+          render json: ncs.containable, status: :created
+        else
+          render json: ncs.errors.to_json, status: :unprocessable_entity
+        end
+      end
+
       def update
         core_node = CoreNode.find(params[:id])
         core_node.update_attributes(core_node_params)
@@ -27,7 +40,7 @@ module Api
       private
 
       def core_node_params
-        params.require(:core_node).permit(:name)
+        params.require(:core_node).permit(:name, :status_id, :city_id, :location_id)
       end
 
     end
