24da2931d7f890e8091e3cccb58d3ae2bb358ce6
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-03-30 20:08:41 +0200

    feat: add missing core node controller spec

---
+++ rails/spec/controllers/api/v1/core_nodes_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::CoreNodesController do
+  let(:object_key) {:core_node}
+
+  before(:each) do
+    @core_node = create_core_node
+    sign_in( :user )
+  end
+
+  describe 'GET /api/v1/core_nodes' do
+    it_should_behave_like "a listable resource"
+  end
+
+  describe 'GET /api/v1/core_nodes/:id' do
+    let(:id) {@core_node.id}
+
+    it_should_behave_like "a viewable resource"
+  end
+
+  describe 'PUT /api/v1/core_nodes/:id' do
+    let(:amendable_key) {:name}
+    let(:amendable_value) {"Oh look! a name..."}
+    let(:id) {@core_node.id}
+
+    it_should_behave_like "an amendable resource"
+  end
+
+  describe 'DELETE /api/v1/core_nodes/:id' do
+    let(:id) {@core_node.id}
+
+    it_should_behave_like "an erasable resource"
+  end
+
+  describe 'POST /api/v1/core_nodes' do
+
+    let(:hash) {
+      attrs = FactoryGirl.attributes_for(:core_node)
+      attrs
+    }
+
+    it_should_behave_like "a createable resource"
+  end
+end
+
