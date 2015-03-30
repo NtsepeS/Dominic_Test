7a1e4abb171ce4bc17625c087173e920fb603616
Nilesh Nathoo <nilesh.nathoo@gmail.com>
2015-03-30 14:51:38 +0200

    feat: add missing bsu controller spec & update bsu create action

--- rails/app/controllers/api/v1/base_station_units_controller.rb
+++ rails/app/controllers/api/v1/base_station_units_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class BaseStationUnitsController < AuthenticatedController
       include ExcelGenerator
+      include ParentContainerParams
 
       def index
         base_station_units = BaseStationUnit.all
@@ -11,6 +12,19 @@ module Api
       def show
         base_station_unit = BaseStationUnit.find(params[:id])
         render json: base_station_unit
+      end
+
+      def create
+        container = parent_container(:base_station_unit, :core_node_id)
+
+        bsu = BaseStationUnit.new(base_station_unit_params)
+        ncs = NewContainerService.new.create(bsu, in: container)
+
+        if ncs.successful?
+          render json: ncs.containable, status: :created
+        else
+          render json: ncs.errors.to_json, status: :unprocessable_entity
+        end
       end
 
       def update
@@ -35,7 +49,7 @@ module Api
       private
 
       def base_station_unit_params
-        params.require(:base_station_unit).permit(:name)
+        params.require(:base_station_unit).permit(:name, :status)
       end
 
     end
