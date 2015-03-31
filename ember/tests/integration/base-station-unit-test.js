aa359f1b023fbe348a2c9f3c5820f1a014298b43
Alecia Bester <alecia.bester@gmail.com>
2015-03-31 19:42:19 +0200

    Integration test: base station unit read

--- ember/tests/integration/base-station-unit-test.js
+++ ember/tests/integration/base-station-unit-test.js
@@ -31,3 +31,21 @@ test('visiting /base-station-units should show a list of links', function(assert
   });
 });
 
+test('visiting /base-station-units/:id should show base station unit details', function(assert) {
+  visit('/base-station-units');
+  var _this = this,
+      baseStationUnitName;
+
+  andThen(function() {
+    baseStationUnitName = find(".base-station-list-item__name:eq(0)").text();
+  });
+
+  click(".base-station-list-item__name:eq(0)");
+
+  andThen(function(){
+    assert.equal(currentPath(), 'base-station-unit.index');
+
+    var new_name = find(".base-station-unit__name").text();
+    assert.equal( new_name.indexOf(baseStationUnitName) > -1, true);
+  });
+});
