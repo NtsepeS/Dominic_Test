9bfa7c25aa22434f3d72c7d6a44d6f9eba36fd5b
Alecia Bester <alecia.bester@gmail.com>
2015-03-31 19:57:31 +0200

    Integration test: base station unit new

--- ember/tests/integration/base-station-unit-test.js
+++ ember/tests/integration/base-station-unit-test.js
@@ -33,8 +33,7 @@ test('visiting /base-station-units should show a list of links', function(assert
 
 test('visiting /base-station-units/:id should show base station unit details', function(assert) {
   visit('/base-station-units');
-  var _this = this,
-      baseStationUnitName;
+  var baseStationUnitName;
 
   andThen(function() {
     baseStationUnitName = find(".base-station-list-item__name:eq(0)").text();
@@ -47,5 +46,24 @@ test('visiting /base-station-units/:id should show base station unit details', f
 
     var new_name = find(".base-station-unit__name").text();
     assert.equal( new_name.indexOf(baseStationUnitName) > -1, true);
+  });
+});
+
+test('visiting /base-station-units/new should create a new base station unit', function(assert) {
+  visit('/base-station-units/new');
+  var newName = "Some awesome base station unit name";
+
+  andThen(function() {
+    fillIn('.base-station-unit__name', newName);
+    fillIn('.base-station-unit__status', 1);
+    fillIn('.base-station-unit__core-node', 1);
+  });
+
+  click('.qa-save');
+
+  andThen(function() {
+    assert.equal(currentPath(), 'base-station-unit.index');
+
+    assert.equal(find('.base-station-unit__name').text().indexOf(newName) > -1, true);
   });
 });
