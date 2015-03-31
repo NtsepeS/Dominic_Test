3440293d33322875f91f2c12bb4e4aad999cb339
Pieter van der Merwe <vandermerwe.pieter@gmail.com>
2015-03-31 14:46:39 +0200

    Change Antenna initialisation back to the old model

--- rails/db/seeds/antennas.seeds.rb
+++ rails/db/seeds/antennas.seeds.rb
@@ -1,2 +1,2 @@
-Antenna.where(size: "30cm", serial_number: 1234, is_asset_tag: 5678, latitude: "-26.150553", longitude: "28.202592").first_or_create
-Antenna.where(size: "30cm", serial_number: 110720023, is_asset_tag: 116034, latitude: "-26.042568", longitude: "28.018240").first_or_create
+Antenna.find_or_create_by(size: "30cm", item_code: 'abc', location_id: 1)
+Antenna.find_or_create_by(size: "30cm",  item_code: 'def', location_id: 2)
