e70df1e09461be9d7914da1605092651d2b4f025
Pieter van der Merwe <vandermerwe.pieter@gmail.com>
2015-04-01 09:47:14 +0200

    Change seed files not to destroy existing data so that it can be run with the production_init task: $ rake db:seed:production_init            # Load the seed data from db/seeds.rb, db/seeds/*.seeds.rb and db/seeds/production_init/*.seeds.rb

--- rails/db/seeds/cities.seeds.rb
+++ rails/db/seeds/cities.seeds.rb
@@ -1,6 +1,5 @@
 require 'csv'
 
-City.delete_all
 CSV.foreach(File.join(File.dirname(__FILE__), "constant_data", "cities.csv"), :headers => true, :col_sep => ';') do |row|
-  City.create(name: row["Title"])
+  City.find_or_create_by(name: row["Title"])
 end
--- rails/db/seeds/clients.seeds.rb
+++ rails/db/seeds/clients.seeds.rb
@@ -1,6 +1,5 @@
 require 'csv'
 
-Client.delete_all
 CSV.foreach(File.join(File.dirname(__FILE__), "constant_data", "clients.csv"), :headers => true, :col_sep => ';') do |row|
-  Client.create(name: row["Title"])
+  Client.find_or_create_by(name: row["Title"])
 end
